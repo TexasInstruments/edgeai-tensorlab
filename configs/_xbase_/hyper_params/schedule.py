78d4255edfc05b1fefe28ed662465d90d6356796
Manu Mathew <a0393608@ti.com>
2020-08-05 22:15:55 +0530

    mmcv has change the lr_updater name to CosineAnnealing. using warmup_iters since warmup_by_epoch is not working now.

--- configs/_xbase_/hyper_params/schedule.py
+++ configs/_xbase_/hyper_params/schedule.py
@@ -6,15 +6,15 @@ optimizer = dict(type='SGD', lr=1e-2, momentum=0.9, weight_decay=1e-4)
 #optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
-# warmup by iterations
-# warmup_cfg = dict(warmup='linear', warmup_iters=500, warmup_ratio=1e-3)
 # warmup by epoch
-warmup_cfg = dict(warmup='linear', warmup_by_epoch=True, warmup_iters=1, warmup_ratio=1e-4)
+# warmup_cfg = dict(warmup='linear', warmup_by_epoch=True, warmup_iters=1, warmup_ratio=1e-4)
+# warmup by iterations
+warmup_cfg = dict(warmup='linear', warmup_iters=1000, warmup_ratio=1e-3)
 
 # lr policy - step
 # lr_config = dict(policy='step', step=[40, 55], **warmup_cfg)
 # lr policy - cosine
-lr_config = dict(policy='CosineAnealing', min_lr_ratio=1e-4, **warmup_cfg)
+lr_config = dict(policy='CosineAnnealing', min_lr_ratio=1e-4, **warmup_cfg)
 
 # runtime settings
 total_epochs = 60
