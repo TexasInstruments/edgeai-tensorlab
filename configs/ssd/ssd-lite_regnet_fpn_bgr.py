89cdbf7109d92336d95b32b5cc51f3950d70f049
Manu Mathew <a0393608@ti.com>
2020-09-12 12:32:26 +0530

    bifpn channels update

--- configs/ssd/ssd-lite_regnet_fpn_bgr.py
+++ configs/ssd/ssd-lite_regnet_fpn_bgr.py
@@ -33,7 +33,7 @@ to_rgb = False                                   # pycls regnet backbones are tr
 
 decoder_fpn_type = 'FPNLite'                    # 'FPNLite' #'BiFPNLite' #'FPN'
 decoder_conv_type = 'ConvDWSep'                 # 'ConvDWSep' #'ConvDWTripletRes' #'ConvDWTripletAlwaysRes'
-fpn_width_fact = 2 if decoder_fpn_type == 'BiFPNLite' else 4
+fpn_width_fact = 1 if decoder_fpn_type == 'BiFPNLite' else 4
 decoder_width_fact = 4
 decoder_depth_fact = 4
 
@@ -45,12 +45,12 @@ regnet_settings = {
                      'pretrained':'open-mmlab://regnetx_800mf'},
     'regnetx_1.6gf':{'bacbone_out_channels':[72, 168, 408, 912], 'group_size_dw':24,
                      'fpn_intermediate_channels':min(96*fpn_width_fact,264),
-                     'fpn_out_channels':min(96*decoder_width_fact,264),
+                     'fpn_out_channels':min(72*decoder_width_fact,264),
                      'fpn_num_blocks':decoder_depth_fact,
                      'pretrained':'open-mmlab://regnetx_1.6gf'},
     'regnetx_3.2gf':{'bacbone_out_channels':[96, 192, 432, 1008], 'group_size_dw':48,
                      'fpn_intermediate_channels':min(120*fpn_width_fact,288),
-                     'fpn_out_channels':min(120*decoder_width_fact,288),
+                     'fpn_out_channels':min(96*decoder_width_fact,288),
                      'fpn_num_blocks':decoder_depth_fact,
                      'pretrained': 'open-mmlab://regnetx_3.2gf'}
 }
