d3891e2e94675f46bd4f3e3659392b4f9b443275
Manu Mathew <a0393608@ti.com>
2021-01-16 00:37:55 +0530

    correcting yolo config

--- configs/yolo/yolov3_d53.py
+++ configs/yolo/yolov3_d53.py
@@ -8,7 +8,7 @@ img_norm_cfg = dict(mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True)
 _base_ = [
     f'../_xbase_/datasets/{dataset_type.lower()}.py',
     '../_xbase_/hyper_params/common_config.py',
-    '../_xbase_/hyper_params/ssd_config.py',
+    '../_xbase_/hyper_params/yolo_config.py',
     '../_xbase_/hyper_params/schedule.py',
 ]
 
@@ -18,6 +18,7 @@ _base_ = [
 quantize = False #'training' #'calibration'
 initial_learning_rate = 8e-2
 samples_per_gpu = 8
+
 if quantize:
   load_from = './work_dirs/yolov3_d53/latest.pth'
   optimizer = dict(type='SGD', lr=initial_learning_rate/100.0, momentum=0.9, weight_decay=4e-5) #1e-4 => 4e-5
@@ -67,22 +68,11 @@ model = dict(
             reduction='sum'),
         loss_wh=dict(type='MSELoss', loss_weight=2.0, reduction='sum')))
 
-# training and testing settings
-train_cfg = dict(_delete_=True,
-    assigner=dict(type='GridAssigner', pos_iou_thr=0.5, neg_iou_thr=0.5, min_pos_iou=0))
-test_cfg = dict(_delete_=True,
-    nms_pre=1000,
-    min_bbox_size=0,
-    score_thr=0.05,
-    conf_thr=0.005,
-    nms=dict(type='nms', iou_thr=0.45),
-    max_per_img=100)
-
 # dataset settings
 train_pipeline = [
     dict(type='LoadImageFromFile', to_float32=True),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='PhotoMetricDistortion'),
+    dict(type='PhotoMetricDistortion') if not quantize else dict(type='Bypass'),
     dict(
         type='Expand',
         mean=img_norm_cfg['mean'],
@@ -92,14 +82,13 @@ train_pipeline = [
         type='MinIoURandomCrop',
         min_ious=(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
         min_crop_size=0.3),
-    dict(type='Resize', img_scale=[(320, 320), input_size], keep_ratio=False),
+    dict(type='Resize', img_scale=input_size, keep_ratio=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=input_size_divisor),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
-
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
