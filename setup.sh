af3f95d2ccd84b0095444a829f2a93f292d00a17
Manu Mathew <a0393608@ti.com>
2021-01-20 11:33:48 +0530

    setup updated

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 echo "mmdetection is not installing cython properly - it has to be installed from conda if this is conda python"
-conda install cython
+conda install -y cython
 
 echo "Installing mmcv - Please check your CUDA and PyTorch versions and modify this link appropriately."
 echo "More info is available in https://github.com/open-mmlab/mmcv and https://github.com/open-mmlab/mmdetection"
-pip install mmcv-full==latest+torch1.6.0+cu102 -f https://openmmlab.oss-accelerate.aliyuncs.com/mmcv/dist/index.html
+pip install --force-reinstall mmcv-full==1.2.5 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.7.0/index.html
 
 echo "Installing mmdetection"
-pip install git+https://github.com/open-mmlab/mmdetection.git
+pip install git+https://github.com/open-mmlab/mmdetection.git@v2.8.0
 
 echo "Please visit https://git.ti.com/cgit/jacinto-ai/pytorch-jacinto-ai-devkit/about/"
 echo "and https://git.ti.com/cgit/jacinto-ai/pytorch-jacinto-ai-devkit/ to clone and install that package."
