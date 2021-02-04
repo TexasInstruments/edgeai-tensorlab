6126450b26541fd165afcfbb58f304ca7d3bc686
Manu Mathew <mathew.manu@ti.com>
2021-02-04 13:42:23 +0530

    using deque instead of list in Parallel

--- jacinto_ai_benchmark/utils/parallel.py
+++ jacinto_ai_benchmark/utils/parallel.py
@@ -1,13 +1,14 @@
 from multiprocessing import Process
+from collections import deque
 
 class Parallel:
     def __init__(self, num_processes):
         self.num_processes = num_processes
-        self.queued_tasks = []
+        self.queued_tasks = deque()
         self.running_processes = [None for _ in range(self.num_processes)]
 
     def __del__(self):
-        for p_idx, proc in enumerate(self.running_processes):
+        for proc_idx, proc in enumerate(self.running_processes):
             if proc is not None:
                 proc.terminate()
                 proc.join()
@@ -28,7 +29,7 @@ class Parallel:
             if (self._num_running() < self.num_processes) and (self._num_queued() > 0):
                 slot_idx = self._empty_slot()
                 assert (slot_idx is not None), 'empty slot expected, but found none'
-                task = self.queued_tasks.pop(0)
+                task = self.queued_tasks.popleft()
                 proc = Process(target=task)
                 self.running_processes[slot_idx] = proc
                 proc.start()
@@ -48,9 +49,9 @@ class Parallel:
         return sum([1 if proc is not None else 0 for proc in self.running_processes])
 
     def _empty_slot(self):
-        for p_idx, proc in enumerate(self.running_processes):
+        for proc_idx, proc in enumerate(self.running_processes):
             if proc is None:
-                return p_idx
+                return proc_idx
             #
         #
         return None
