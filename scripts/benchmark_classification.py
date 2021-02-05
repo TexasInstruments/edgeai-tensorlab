adb8e8f49b160c89e854dadf405509b635039206
Manu Mathew <mathew.manu@ti.com>
2021-02-05 12:00:20 +0530

    model paths updated

--- scripts/benchmark_classification.py
+++ scripts/benchmark_classification.py
@@ -85,7 +85,7 @@ pipeline_configs = [
     utils.dict_update(pipeline_cfg, {
         'preprocess':config.get_preproc_vgg(),
         'session':sessions.TVMDLRSession(**config.session_tvm_dlr_cfg, work_dir=work_dir,
-            model_path=f'{config.modelzoo_path}/edge/classification/imagenet1k/torchvision/shufflenetv2_x1p0_opset9.onnx',
+            model_path=f'{config.modelzoo_path}/edge/classification/imagenet1k/torchvision/shufflenetv2_x1.0_opset9.onnx',
             input_shape={'input.1': (1, 3, 224, 224)})
     }),
     # torchvision: classification mobilenetv2_224x224 expected_metric: 71.88% top-1 accuracy
