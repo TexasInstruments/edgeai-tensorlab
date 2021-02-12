9d2fda9ebea1d3b38c675bfcf3380c86fb182c28
Manu Mathew <mathew.manu@ti.com>
2021-02-12 21:22:15 +0530

    tqdm fix for not following miniters

--- jacinto_ai_benchmark/utils/progress_indicator.py
+++ jacinto_ai_benchmark/utils/progress_indicator.py
@@ -5,11 +5,10 @@ from colorama import Fore
 __all__ = ['progress_indicator']
 
 
-def progress_indicator(iterable, desc):
-    desc_len = 80
-    desc = desc[:desc_len] if len(desc) > desc_len else desc
+def progress_indicator(iterable, desc, desc_len=80):
+    desc = desc[:desc_len] if (desc_len is not None and len(desc) > desc_len) else desc
     miniters = max(len(iterable)//100, 1)
-    colors = (Fore.GREEN, Fore.WHITE, Fore.YELLOW, Fore.CYAN, Fore.RESET)
-    bar_format = '%s{desc:80}|%s{percentage:4.0f}%%|%s{bar:10}|%s{r_bar}%s' % colors
+    format_arg = (Fore.GREEN, desc_len, Fore.WHITE, Fore.YELLOW, Fore.CYAN, Fore.RESET)
+    bar_format = '%s{desc:%s}|%s{percentage:4.0f}%%|%s{bar:10}|%s{r_bar}%s' % format_arg
     return tqdm(iterable=iterable, desc=desc, bar_format=bar_format, file=sys.stdout,
-                miniters=miniters, leave=True)
+                miniters=miniters, position=0, leave=True)
