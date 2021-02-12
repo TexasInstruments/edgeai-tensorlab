11ea3472ce23023e8ab8237935e42d3978ce71a0
Manu Mathew <mathew.manu@ti.com>
2021-02-12 15:45:28 +0530

    progress indicator update

--- jacinto_ai_benchmark/utils/progress_indicator.py
+++ jacinto_ai_benchmark/utils/progress_indicator.py
@@ -12,6 +12,6 @@ def progress_indicator(iterable, desc):
     desc = desc[:desc_len] if len(desc) > desc_len else desc
     miniters = max(len(iterable)//100, 1)
     colors = (Fore.GREEN, Fore.WHITE, Fore.YELLOW, Fore.CYAN, Fore.RESET)
-    bar_format = '%s{desc:75}|%s{percentage:4.0f}%%|%s{bar:25}|%s{r_bar}%s' % colors
+    bar_format = '%s{desc:80}|%s{percentage:4.0f}%%|%s{bar:10}|%s{r_bar}%s' % colors
     return tqdm(iterable=iterable, desc=desc, bar_format=bar_format, file=sys.stdout,
                 miniters=miniters, leave=True)
