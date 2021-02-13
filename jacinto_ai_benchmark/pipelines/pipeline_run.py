1d05ab57b779044e628bf3979be54bf0fe086062
Manu Mathew <mathew.manu@ti.com>
2021-02-13 12:10:21 +0530

    better minitoring of tasks

--- jacinto_ai_benchmark/pipelines/pipeline_run.py
+++ jacinto_ai_benchmark/pipelines/pipeline_run.py
@@ -15,10 +15,13 @@ def run(pipeline_configs, perfsim=False, parallel_devices=None):
 def _run_pipelines_sequential(pipeline_configs, perfsim=False):
     # get the cwd so that we can continue even if exception occurs
     cwd = os.getcwd()
+    results_list = []
     for pipeline_config in utils.progress_step(pipeline_configs, desc='tasks'):
         os.chdir(cwd)
-        _run_pipeline_with_log(pipeline_config, perfsim)
+        result = _run_pipeline_with_log(pipeline_config, perfsim)
+        results_list.append(result)
     #
+    return results_list
 
 
 def _run_pipelines_parallel(pipeline_configs, perfsim=False, parallel_devices=None):
@@ -34,8 +37,8 @@ def _run_pipelines_parallel(pipeline_configs, perfsim=False, parallel_devices=No
                                                     perfsim, parallel_device=parallel_device)
         parallel_exec.enqueue(run_pipeline_bound_func)
     #
-    parallel_exec.start()
-    parallel_exec.wait()
+    results_list = parallel_exec.run()
+    return results_list
 
 
 def _run_pipeline_with_log(pipeline_config, perfsim=False, parallel_device=None):
@@ -47,9 +50,9 @@ def _run_pipeline_with_log(pipeline_config, perfsim=False, parallel_device=None)
     os.makedirs(work_dir, exist_ok=True)
     logger = utils.TeeLogger(os.path.join(work_dir, 'run.log'))
 
-    results = None
+    result = None
     try:
-        results = run_pipeline(pipeline_config)
+        result = run_pipeline(pipeline_config)
     except Exception as e:
         print(f'\n{str(e)}')
     #
@@ -64,7 +67,7 @@ def _run_pipeline_with_log(pipeline_config, perfsim=False, parallel_device=None)
     #     results.update(perfsim_dict)
     # #
 
-    results_message = f'\nBenchmarkResults: {results}'
+    results_message = f'\nBenchmarkResults: {result}'
     if pipeline_config['verbose_mode']:
         print(results_message)
     else:
@@ -72,6 +75,7 @@ def _run_pipeline_with_log(pipeline_config, perfsim=False, parallel_device=None)
     #
     logger.close()
     logger = None
+    return result
 
 
 def run_pipeline(pipeline_config):
@@ -81,10 +85,10 @@ def run_pipeline(pipeline_config):
     pipeline_type = pipeline_config['type']
     if pipeline_type == 'accuracy':
         accuracy_pipeline = AccuracyPipeline()
-        results = accuracy_pipeline.run(pipeline_config)
+        result = accuracy_pipeline.run(pipeline_config)
     else:
         assert False, f'unknown pipeline: {pipeline_type}'
     #
-    return results
+    return result
 
 
--- jacinto_ai_benchmark/utils/parallel_run.py
+++ jacinto_ai_benchmark/utils/parallel_run.py
@@ -1,5 +1,9 @@
 import multiprocessing
 import collections
+import time
+import traceback
+import logging
+from colorama import Fore
 from .progress_step import *
 
 
@@ -11,25 +15,17 @@ class ParallelRun:
     def enqueue(self, task):
         self.queued_tasks.append(task)
 
-    def start(self):
+    def run(self):
         assert len(self.queued_tasks) > 0, f'at least one task must be queued, got {len(self.queued_tasks)}'
-        if len(self.queued_tasks) == 1 or self.num_processes <= 1:
-            self._run_sequential()
-        else:
-            self._run_parallel()
-        #
-
-    def wait(self):
-        pass
+        return self._run_parallel()
 
     def _run_sequential(self):
+        result_list = []
         for task_id, task in progress_step(self.queued_tasks, desc='tasks'):
-            task()
+            result = task()
+            result_list.append(result)
         #
-
-    def _worker(self, task):
-        result = task()
-        return result
+        return result_list
 
     def _run_parallel(self):
         num_tasks = len(self.queued_tasks)
@@ -37,9 +33,54 @@ class ParallelRun:
         process_pool = multiprocessing.Pool(self.num_processes)
         results = process_pool.imap_unordered(self._worker, self.queued_tasks)
         # monitor the progress
-        # results does not have len - explicitly give total
-        for result in progress_step(results, desc='tasks', total=num_tasks):
-            pass
-        #
+        result_list = self._run_monitor(results, num_tasks)
+        return result_list
 
+    def _run_monitor(self, result_iterator, num_tasks, timeout=60.0, verbose=True):
+        num_completed = 0
+        time_taken_str = ''
+        eta_str = ''
+        it_per_sec = 0
+        result_list = []
+        start_time = time.time()
+        while num_completed < num_tasks:
+            try:
+                result = result_iterator.__next__(timeout=timeout)
+                result_list.append(result)
+                num_completed += 1
+                end_time = time.time()
+                time_taken = (end_time - start_time)
+                time_taken_str = self._delta_time_string(time_taken)
+                eta = (time_taken / num_completed) * (num_tasks - num_completed)
+                eta_str = self._delta_time_string(eta)
+                it_per_sec = (num_completed / time_taken) if time_taken != 0 else 0.0
+            except KeyboardInterrupt as e:
+                logging.error(traceback.format_exc())
+                return result_list
+            except ValueError as e:
+                logging.error(traceback.format_exc())
+                return result_list
+            except Exception as e:
+                pass
+            #
+            if verbose:
+                print(f" {Fore.MAGENTA}tasks: {num_completed}/{num_tasks} |"
+                      f" {Fore.YELLOW}[{time_taken_str}<{eta_str},{it_per_sec:6.2f}it/s]{Fore.RESET}",
+                      end=None)
+            #
+        #
+        return result_list
+
+    def _delta_time_string(self, seconds):
+        days = int(seconds//(60*60*24))
+        seconds = int(seconds%(60*60*24))
+        hours = int(seconds//(60*60))
+        seconds = int(seconds%(60*60))
+        minutes = int(seconds//(60))
+        # seconds = int(seconds%(60))
+        return f'{days}d,{hours:02d}:{minutes:02d}'
+
+    def _worker(self, task):
+        result = task()
+        return result
 
