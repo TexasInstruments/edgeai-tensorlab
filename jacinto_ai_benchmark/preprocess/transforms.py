fd64a2db2bdb264c3a8d64406c4bd7b798bef3f8
Manu Mathew <mathew.manu@ti.com>
2021-02-16 18:59:08 +0530

    bugfix in transforms

--- jacinto_ai_benchmark/preprocess/transforms.py
+++ jacinto_ai_benchmark/preprocess/transforms.py
@@ -29,9 +29,9 @@ class ImageRead(object):
             self.info['preprocess']['image_shape'] = img.size[1], img.size[0], len(img.getbands())
         elif self.backend == 'cv2':
             img = cv2.imread(path)
-            if img.shape[3] == 1:
+            if img.shape[-1] == 1:
                 img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-            elif img.shape[3] == 4:
+            elif img.shape[-1] == 4:
                 img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
             #
             # always return in RGB format
