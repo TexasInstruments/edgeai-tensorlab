ca65cf05997f25139dadd172cdb8b8ba55f986ac
Manu Mathew <mathew.manu@ti.com>
2021-02-19 19:05:06 +0530

    run_pipeline is now a classmethod of PipelineRunner

--- jacinto_ai_benchmark/pipelines/pipeline_runner.py
+++ jacinto_ai_benchmark/pipelines/pipeline_runner.py
@@ -2,38 +2,6 @@ import functools
 from .accuracy_pipeline import *
 from .. import utils
 
-# this run_pipeline cannot be part of PipelineRunner as it causes an
-# error during pickling involved in the launch of a process.
-# keeping it outside.
-def run_pipeline(pipeline_config, parallel_device=None, description=''):
-    if parallel_device is not None:
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(parallel_device)
-    #
-    result = {}
-    pipeline_types = utils.as_list(pipeline_config['type'])
-    try:
-        for pipeline_type in pipeline_types:
-            if pipeline_type == 'accuracy':
-                # use with statement, so that the logger and other file resources are cleaned up
-                with AccuracyPipeline(pipeline_config) as accuracy_pipeline:
-                    accuracy_result = accuracy_pipeline.run(description)
-                    result.update(accuracy_result)
-                #
-            elif pipeline_type == 'something':
-                # this is just an example of how other pipelines can be implemented.
-                # 'something' used here is not real and it is not supported
-                with SomethingPipeline(pipeline_config) as something_pipeline:
-                    something_result = something_pipeline.run()
-                    result.update(something_result)
-                #
-            else:
-                assert False, f'unknown pipeline: {pipeline_type}'
-            #
-    except Exception as e:
-        print(f'\n{str(e)}')
-    #
-    return result
-
 
 class PipelineRunner():
     def __init__(self, settings, pipeline_configs):
@@ -70,7 +38,7 @@ class PipelineRunner():
         for pipeline_id, pipeline_config in enumerate(self.pipeline_configs.values()):
             os.chdir(cwd)
             description = f'{pipeline_id+1}/{total}'
-            result = run_pipeline(pipeline_config, description=description)
+            result = self._run_pipeline(pipeline_config, description=description)
             results_list.append(result)
         #
         return results_list
@@ -85,12 +53,44 @@ class PipelineRunner():
             os.chdir(cwd)
             parallel_device = self.settings.parallel_devices[pipeline_index % num_devices] \
                 if self.settings.parallel_devices is not None else 0
-            run_pipeline_bound_func = functools.partial(run_pipeline, pipeline_config,
+            run_pipeline_bound_func = functools.partial(self._run_pipeline, pipeline_config,
                                     parallel_device=parallel_device, description='')
             parallel_exec.enqueue(run_pipeline_bound_func)
         #
         results_list = parallel_exec.run()
         return results_list
+
+    # this function cannot be an instance method of PipelineRunner, as it causes an
+    # error during pickling, involved in the launch of a process is parallel run. make it classmethod
+    @classmethod
+    def _run_pipeline(cls, pipeline_config, parallel_device=None, description=''):
+        if parallel_device is not None:
+            os.environ['CUDA_VISIBLE_DEVICES'] = str(parallel_device)
+        #
+        result = {}
+        pipeline_types = utils.as_list(pipeline_config['type'])
+        try:
+            for pipeline_type in pipeline_types:
+                if pipeline_type == 'accuracy':
+                    # use with statement, so that the logger and other file resources are cleaned up
+                    with AccuracyPipeline(pipeline_config) as accuracy_pipeline:
+                        accuracy_result = accuracy_pipeline.run(description)
+                        result.update(accuracy_result)
+                    #
+                elif pipeline_type == 'something':
+                    # this is just an example of how other pipelines can be implemented.
+                    # 'something' used here is not real and it is not supported
+                    with SomethingPipeline(pipeline_config) as something_pipeline:
+                        something_result = something_pipeline.run()
+                        result.update(something_result)
+                    #
+                else:
+                    assert False, f'unknown pipeline: {pipeline_type}'
+                #
+        except Exception as e:
+            print(f'\n{str(e)}')
+        #
+        return result
 
     def _check_model_selection(self, config, pipeline_config):
         if config.model_selection is not None:
