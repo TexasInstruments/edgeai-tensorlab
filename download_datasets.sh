964b4aa4d01982bb206c705c7f765644075ce42f
Manu Mathew <mathew.manu@ti.com>
2021-02-26 16:37:41 +0530

    download datasets - VOC2012

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,5 +1,5 @@
 
-downloads_folder=dependencies/downloads
+downloads_folder=./dependencies/downloads
 
 #######################################################
 # ImageNet Dataset
@@ -11,10 +11,10 @@ mkdir -p $imagenet_folder
 imagenet_folder=$downloads_folder/imagenet
 
 imagenet_tar=$downloads_folder/ILSVRC2012_img_val.tar
-wget http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_val.tar -O $imagenet_tar
+wget --show-progress http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_val.tar -O $imagenet_tar
 tar -xf $imagenet_tar -C $imagenet_folder/val
 
-wget -c http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz -O $downloads_folder/caffe_ilsvrc12.tar.gz
+wget --show-progress http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz -O $downloads_folder/caffe_ilsvrc12.tar.gz
 tar -xf $downloads_folder/caffe_ilsvrc12.tar.gz -C $imagenet_folder
 
 #######################################################
@@ -23,10 +23,10 @@ echo "Downloading the COCO dataset"
 mkdir -p $downloads_folder/coco/val2017
 mkdir -p $downloads_folder/coco/annotations
 
-wget http://images.cocodataset.org/zips/val2017.zip -O $downloads_folder/val2017.zip
+wget --show-progress http://images.cocodataset.org/zips/val2017.zip -O $downloads_folder/val2017.zip
 unzip -q $downloads_folder/val2017.zip -d $downloads_folder/coco/val2017
 
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O $downloads_folder/annotations_trainval2017.zip
+wget --show-progress http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O $downloads_folder/annotations_trainval2017.zip
 unzip -q $downloads_folder/annotations_trainval2017.zip -d $downloads_folder/coco/annotations
 
 #######################################################
@@ -34,7 +34,7 @@ unzip -q $downloads_folder/annotations_trainval2017.zip -d $downloads_folder/coc
 echo "Downloading the ADE20K dataset"
 mkdir -p $downloads_folder/ADEChallengeData2016
 
-wget http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip -O $downloads_folder/ADEChallengeData2016.zip
+wget --show-progress http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip -O $downloads_folder/ADEChallengeData2016.zip
 unzip -q $downloads_folder/ADEChallengeData2016.zip -d $downloads_folder/ADEChallengeData2016
 
 #######################################################
@@ -44,4 +44,9 @@ wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCdevkit_18-May-2011.tar -o
 tar -xf $downloads_folder/VOCdevkit_18-May-2011.tar -C $downloads_folder/VOCdevkit
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar -o $downloads_folder/VOCtrainval_11-May-2012.tar
 tar -xf $downloads_folder/VOCtrainval_11-May-2012.tar -C $downloads_folder/VOCdevkit/VOC2012
-
+## alternate URL - this is untested
+##wget --show-progress https://data.deepai.org/PascalVOC2012.zip -o $downloads_folder/PascalVOC2012.zip
+##unzip -q $downloads_folder/PascalVOC2012.zip -d $downloads_folder/VOCdevkit
+python $downloads_folder/remove_gt_colormap_voc.py \
+      --original_gt_folder "${downloads_folder}/VOCdevkit/VOC2012/SegmentationClass" \
+      --output_dir "${downloads_folder}/VOCdevkit/VOC2012/SegmentationClassRaw"
