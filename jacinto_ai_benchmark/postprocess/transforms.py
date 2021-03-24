6e0c19f0931db6e7ad5bcda4050d9b5b9932510c
Manu Mathew <mathew.manu@ti.com>
2021-03-24 14:55:04 +0530

    handle int64 resize in postprocess/transforms

--- jacinto_ai_benchmark/postprocess/transforms.py
+++ jacinto_ai_benchmark/postprocess/transforms.py
@@ -102,6 +102,9 @@ class ShuffleList():
 class SegmentationImageResize():
     def __call__(self, label, info_dict):
         image_shape = info_dict['data_shape']
+        if label.dtype in (np.int32, np.int64):
+            label = label.astype(np.float32)
+        #
         label = cv2.resize(label, dsize=(image_shape[1],image_shape[0]), interpolation=cv2.INTER_NEAREST)
         return label, info_dict
 
