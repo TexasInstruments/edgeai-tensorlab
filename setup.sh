3c5a1ef177299ec218abd6033bb0cb7b9b5fab57
Manu Mathew <mathew.manu@ti.com>
2021-05-26 21:07:58 +0530

    setup.sh - add pytorch and torchvision versions

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 echo "mmdetection is not installing cython properly - it has to be installed from conda if this is conda python"
 conda install -y cython
 
+echo "Installing pytorch and torchvision"
+pip install torch==1.6.0 torchvision=0.7.0
+
 echo "Installing mmcv - Please check your CUDA and PyTorch versions and modify this link appropriately."
 echo "More info is available in https://github.com/open-mmlab/mmcv and https://github.com/open-mmlab/mmdetection"
-pip install --force-reinstall mmcv-full==1.2.5 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.7.0/index.html
+pip install --force-reinstall mmcv-full==1.2.5 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.6.0/index.html
 
 echo "Installing mmdetection"
 pip install git+https://github.com/open-mmlab/mmdetection.git@v2.8.0
