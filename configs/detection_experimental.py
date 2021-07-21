0d01473d3358a1738742613a6ce2a3ac48127438
Manu Mathew <mathew.manu@ti.com>
2021-07-21 11:42:28 +0530

    detection - yolov3 models - added 16bit for output layers

--- configs/detection_experimental.py
+++ configs/detection_experimental.py
@@ -71,9 +71,12 @@ def get_configs(settings, work_dir):
         'vdet-12-020-0':utils.dict_update(common_cfg,
             preprocess=settings.get_preproc_onnx(640, 640,  resize_with_pad=True, mean=(0.0, 0.0, 0.0), scale=(0.003921568627, 0.003921568627, 0.003921568627), backend='cv2', pad_color=[114,114,114]),
             session=onnx_session_type(**common_session_cfg,
-                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), {'object_detection:meta_arch_type': 6, 'object_detection:meta_layers_names_list':f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5s6_640_ti_lite_metaarch.prototxt',
-                                                                                        'advanced_options:output_feature_16bit_names_list':'370, 680, 990, 1300'}),
-                model_path=f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5s6_640_ti_lite_37p4_56p0.onnx'),
+                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(),
+                                    {'object_detection:meta_arch_type': 6,
+                                     'object_detection:meta_layers_names_list':f'../edgeai-yolov5/pretrained_models/models/yolov5s6_640_ti_lite/weights/yolov5s6_640_ti_lite_metaarch.prototxt',
+                                     'advanced_options:output_feature_16bit_names_list':'370, 680, 990, 1300'
+                                     }),
+                model_path=f'../edgeai-yolov5/pretrained_models/models/yolov5s6_640_ti_lite/weights/yolov5s6_640_ti_lite_37p4_56p0.onnx'),
             postprocess=settings.get_postproc_detection_yolov5_onnx(squeeze_axis=None, normalized_detections=False, resize_with_pad=True, formatter=postprocess.DetectionBoxSL2BoxLS()), #TODO: check this
             metric=dict(label_offset_pred=datasets.coco_det_label_offset_80to90(label_offset=1)),
             model_info=dict(metric_reference={'accuracy_ap[.5:.95]%':37.4})
@@ -81,9 +84,12 @@ def get_configs(settings, work_dir):
         'vdet-12-021-0':utils.dict_update(common_cfg,
             preprocess=settings.get_preproc_onnx(384, 384,  resize_with_pad=True, mean=(0.0, 0.0, 0.0), scale=(0.003921568627, 0.003921568627, 0.003921568627), backend='cv2', pad_color=[114,114,114]),
             session=onnx_session_type(**common_session_cfg,
-                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), {'object_detection:meta_arch_type': 6, 'object_detection:meta_layers_names_list':f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5s6_384_ti_lite_metaarch.prototxt',
-                                                                                        'advanced_options:output_feature_16bit_names_list':'168, 370, 680, 990, 1300'}),
-                model_path=f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5s6_384_ti_lite_32p8_51p2.onnx'),
+                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(),
+                                    {'object_detection:meta_arch_type': 6,
+                                     'object_detection:meta_layers_names_list':f'../edgeai-yolov5/pretrained_models/models/yolov5s6_384_ti_lite/weights/yolov5s6_384_ti_lite_metaarch.prototxt',
+                                     'advanced_options:output_feature_16bit_names_list':'168, 370, 680, 990, 1300'
+                                     }),
+                model_path=f'../edgeai-yolov5/pretrained_models/models/yolov5s6_384_ti_lite/weights/yolov5s6_384_ti_lite_32p8_51p2.onnx'),
             postprocess=settings.get_postproc_detection_yolov5_onnx(squeeze_axis=None, normalized_detections=False, resize_with_pad=True, formatter=postprocess.DetectionBoxSL2BoxLS()), #TODO: check this
             metric=dict(label_offset_pred=datasets.coco_det_label_offset_80to90(label_offset=1)),
             model_info=dict(metric_reference={'accuracy_ap[.5:.95]%':32.8})
@@ -91,9 +97,12 @@ def get_configs(settings, work_dir):
         'vdet-12-022-0':utils.dict_update(common_cfg,
             preprocess=settings.get_preproc_onnx(640, 640, resize_with_pad=True, mean=(0.0, 0.0, 0.0), scale=(0.003921568627, 0.003921568627, 0.003921568627), backend='cv2', pad_color=[114,114,114]),
             session=onnx_session_type(**common_session_cfg,
-                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), {'object_detection:meta_arch_type': 6, 'object_detection:meta_layers_names_list':f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5m6_640_ti_lite_metaarch.prototxt',
-                                                                                        'advanced_options:output_feature_16bit_names_list':'228, 498, 808, 1118, 1428'}),
-                model_path=f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5m6_640_ti_lite_44p1_62p9.onnx'),
+                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(),
+                                    {'object_detection:meta_arch_type': 6,
+                                     'object_detection:meta_layers_names_list':f'../edgeai-yolov5/pretrained_models/models/yolov5m6_640_ti_lite/weights/yolov5m6_640_ti_lite_metaarch.prototxt',
+                                     'advanced_options:output_feature_16bit_names_list':'228, 498, 808, 1118, 1428'
+                                     }),
+                model_path=f'../edgeai-yolov5/pretrained_models/models/yolov5m6_640_ti_lite/weights/yolov5m6_640_ti_lite_44p1_62p9.onnx'),
             postprocess=settings.get_postproc_detection_yolov5_onnx(squeeze_axis=None, normalized_detections=False,  resize_with_pad=True, formatter=postprocess.DetectionBoxSL2BoxLS()), #TODO: check this
             metric=dict(label_offset_pred=datasets.coco_det_label_offset_80to90(label_offset=1)),
             model_info=dict(metric_reference={'accuracy_ap[.5:.95]%':44.1})
@@ -101,9 +110,12 @@ def get_configs(settings, work_dir):
            'vdet-12-023-0':utils.dict_update(common_cfg,
             preprocess=settings.get_preproc_onnx(640, 640, resize_with_pad=True, mean=(0.0, 0.0, 0.0), scale=(0.003921568627, 0.003921568627, 0.003921568627), backend='cv2', pad_color=[114,114,114]),
             session=onnx_session_type(**common_session_cfg,
-                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), {'object_detection:meta_arch_type': 6, 'object_detection:meta_layers_names_list':f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5l6_640_ti_lite_metaarch.prototxt',
-                                                                                        'advanced_options:output_feature_16bit_names_list':'288, 626, 936, 1246, 1556'}),
-                model_path=f'{settings.models_path}/vision/detection/coco/ultralytics-yolov5/yolov5l6_640_ti_lite_47p1_65p6.onnx'),
+                runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(),
+                                    {'object_detection:meta_arch_type': 6,
+                                     'object_detection:meta_layers_names_list':f'../edgeai-yolov5/pretrained_models/models/yolov5l6_640_ti_lite/weights/yolov5l6_640_ti_lite_metaarch.prototxt',
+                                     'advanced_options:output_feature_16bit_names_list':'288, 626, 936, 1246, 1556'
+                                     }),
+                model_path=f'../edgeai-yolov5/pretrained_models/models/yolov5l6_640_ti_lite/weights/yolov5l6_640_ti_lite_47p1_65p6.onnx'),
             postprocess=settings.get_postproc_detection_yolov5_onnx(squeeze_axis=None, normalized_detections=False,  resize_with_pad=True, formatter=postprocess.DetectionBoxSL2BoxLS()), #TODO: check this
             metric=dict(label_offset_pred=datasets.coco_det_label_offset_80to90(label_offset=1)),
             model_info=dict(metric_reference={'accuracy_ap[.5:.95]%':47.1})
@@ -137,14 +149,14 @@ def get_configs(settings, work_dir):
         #################################################################
         #       TFLITE MODELS
         #################tflite models###################################
-        'vdet-12-415-0':utils.dict_update(common_cfg,
-            preprocess=settings.get_preproc_tflite((1024,1024), (1024,1024), backend='cv2'),
-            session=tflite_session_type(**common_session_cfg, runtime_options=runtime_options_tflite_np2,
-                model_path=f'{settings.models_path}/vision/detection/coco/tf2-models/ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8.tflite'),
-            postprocess=postproc_detection_tflite,
-            metric=dict(label_offset_pred=datasets.coco_det_label_offset_90to90()),
-            model_info=dict(metric_reference={'accuracy_ap[.5:.95]%':38.3})
-        ),
+        # 'vdet-12-415-0':utils.dict_update(common_cfg,
+        #     preprocess=settings.get_preproc_tflite((1024,1024), (1024,1024), backend='cv2'),
+        #     session=tflite_session_type(**common_session_cfg, runtime_options=runtime_options_tflite_np2,
+        #         model_path=f'{settings.models_path}/vision/detection/coco/tf2-models/ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8.tflite'),
+        #     postprocess=postproc_detection_tflite,
+        #     metric=dict(label_offset_pred=datasets.coco_det_label_offset_90to90()),
+        #     model_info=dict(metric_reference={'accuracy_ap[.5:.95]%':38.3})
+        # ),
     }
     return pipeline_configs
 
