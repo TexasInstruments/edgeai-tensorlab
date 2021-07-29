5273a6ff0f94be18ffcdfd53d915cbd134a690e4
Manu Mathew <mathew.manu@ti.com>
2021-07-30 00:27:58 +0530

    version updated

--- run_create_modelartifacts_links.sh
+++ run_create_modelartifacts_links.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-url=http://software-dl.ti.com/jacinto7/esd/modelzoo/08_00_00_03/modelartifacts/8bits
+url=http://software-dl.ti.com/jacinto7/esd/modelzoo/08_00_00_05/modelartifacts/8bits
 
-for artifact in $(ls -1 ./modelartifacts/8bits |grep .tar.gz)
+for artifact in $(ls -1 ./work_dirs/modelartifacts/8bits |grep .tar.gz)
 do
-echo ${url}/${artifact} > ./modelartifacts/8bits/${artifact}.link
+echo ${url}/${artifact} > ./work_dirs/modelartifacts/8bits/${artifact}.link
 done
