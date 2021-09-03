4995155b6859cc77150c5b430261e4aae3effa9f
Manu Mathew <a0393608@udtensorlab5>
2021-09-03 15:41:28 +0530

    release commit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-echo "-----------------------------------------------------------"
-echo "attempting to install packages required to build pillow-simd"
-echo "if you don't have sudo access, uncomment pillow-simd and enable pillow in requirements.txt and references/requirements.txt"
-echo "also comment out the below line in that case"
-#sudo apt-get install libjpeg-dev zlib1g-dev
+#!/usr/bin/env bash
+
+# Dependencies for building pillow-simd
+echo 'Installing dependencies to build pillow-simd. If you dont have sudo access, comment the below line and replace pillow-simd with pillow in the requirements file'
+sudo apt-get install libjpeg-dev zlib1g-dev
 
 echo "-----------------------------------------------------------"
 echo "installing requirements"
@@ -12,11 +12,10 @@ pip install -r references/requirements.txt
 echo "-----------------------------------------------------------"
 echo "building torchvision"
 # may need pytorch nightly to build this package
-echo "installing pytoch-nightly for cuda 10.2."
+echo "installing pytoch for cuda 11.1"
 echo "if your cuda version is different, please change the pip pytorh nightly url"
 echo "find the url here: https://pytorch.org/get-started/locally/"
-#pip install --pre torch -f https://download.pytorch.org/whl/nightly/cu102/torch_nightly.html
-pip install torch==1.9.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.9.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 
 # an editable install - changes in this local torchvision module immediately takes effect
 pip install -e ./
