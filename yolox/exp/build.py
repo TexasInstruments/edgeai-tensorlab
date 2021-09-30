8c7f557afa94fa2c7d7b399c7589a42f1e1b375b
a0132471 <a0132471@ti.com>
2021-09-30 23:56:47 +0530

    cosmetic changes in build.py for easy merging

--- yolox/exp/build.py
+++ yolox/exp/build.py
@@ -24,10 +24,10 @@ def get_exp_by_name(exp_name):
     filedict = {
         "yolox-s": "yolox_s.py",
         "yolox-s-ti-lite": "yolox_s_ti_lite.py",
-        "yolox-s-human-pose": "yolox_s_human_pose.py",
         "yolox-m": "yolox_m.py",
         "yolox-l": "yolox_l.py",
         "yolox-x": "yolox_x.py",
+        "yolox-s-human-pose": "yolox_s_human_pose.py",
         "yolox-tiny": "yolox_tiny.py",
         "yolox-nano": "nano.py",
         "yolov3": "yolov3.py",
