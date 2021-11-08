b517a57ec173dbae7ec20b20a4273b01042e2287
Manu Mathew <a0393608@udtensorlab5>
2021-11-08 21:24:50 +0530

    minor setup script update

--- setup.sh
+++ setup.sh
@@ -6,13 +6,11 @@ pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 -f 
 
 echo "Installing mmcv - Please check your CUDA and PyTorch versions and modify this link appropriately."
 echo "More info is available in https://github.com/open-mmlab/mmcv and https://github.com/open-mmlab/mmdetection"
-pip3 install --force-reinstall mmcv-full==1.2.7 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
-
-echo "Installing mmdetection"
-pip3 install git+https://github.com/open-mmlab/mmdetection.git@v2.8.0
+pip3 install openmim
+mim install mmdet==2.8.0
+mim install mmcv-full==1.2.7
 
 pip3 install onnxruntime
-pip3 install onnxoptimizer
 
 echo "This package depdends on edgeai-torchvision:"
 echo "Please visit https://github.com/TexasInstruments/edgeai-torchvision and clone and install that package."
