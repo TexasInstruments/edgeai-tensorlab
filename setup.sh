986beac0c2d3062935da0980d394551012a989f1
Manu Mathew <a0393608@udtensorlab5>
2021-11-15 11:30:53 +0530

    cosmetic fixes to setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Dependencies for building pillow-simd
-echo 'Installing dependencies to build pillow-simd. If you dont have sudo access, comment the below line and replace pillow-simd with pillow in the requirements file'
+echo 'Installing dependencies to build pillow-simd'
+echo 'If you do not have sudo access, comment the below line and replace pillow-simd with pillow in the requirements file'
 sudo apt-get install libjpeg-dev zlib1g-dev
 
 echo "-----------------------------------------------------------"
@@ -12,9 +13,8 @@ pip install -r references/requirements.txt
 echo "-----------------------------------------------------------"
 echo "building torchvision"
 # may need pytorch nightly to build this package
-echo "installing pytoch for cuda 11.1"
-echo "if your cuda version is different, please change the pip pytorh nightly url"
-echo "find the url here: https://pytorch.org/get-started/locally/"
+echo "installing pytorch for cuda 11.1"
+echo "other versions can be found here: https://pytorch.org/get-started/locally/"
 pip install torch==1.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 
 # an editable install - changes in this local torchvision module immediately takes effect
