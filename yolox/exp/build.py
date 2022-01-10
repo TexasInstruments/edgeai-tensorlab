0545ef63eaa20bfa2b3447b853873726bb852299
Debapriya Maji <d-maji@ti.com>
2022-01-10 17:22:34 +0530

    Made changes for licensing issue against YOLOv5

--- yolox/exp/build.py
+++ yolox/exp/build.py
@@ -18,30 +18,13 @@ def get_exp_by_file(exp_file):
 
 
 def get_exp_by_name(exp_name):
-    import yolox
-
-    yolox_path = os.path.dirname(os.path.dirname(yolox.__file__))
-    filedict = {
-        "yolox-s": "yolox_s.py",
-        "yolox-s-ti-lite": "yolox_s_ti_lite.py",
-        "yolox-s-object-pose" : "yolox_s_object_pose.py",
-        "yolox-m-ti-lite": "yolox_m_ti_lite.py",
-        "yolox-m": "yolox_m.py",
-        "yolox-l": "yolox_l.py",
-        "yolox-x": "yolox_x.py",
-        "yolox-s-human-pose": "yolox_s_human_pose.py",
-        "yolox-tiny": "yolox_tiny.py",
-        "yolox-tiny-ti-lite": "yolox_tiny_ti_lite.py",
-        "yolox-nano": "nano.py",
-        "yolox-nano-ti-lite": "nano_ti_lite.py",
-        "yolov3": "yolov3.py",
-    }
-    filename = filedict[exp_name]
-    exp_path = os.path.join(yolox_path, "exps", "default", filename)
-    return get_exp_by_file(exp_path)
+    exp = exp_name.replace("-", "_")  # convert string like "yolox-s" to "yolox_s"
+    module_name = ".".join(["yolox", "exp", "default", exp])
+    exp_object = importlib.import_module(module_name).Exp()
+    return exp_object
 
 
-def get_exp(exp_file, exp_name):
+def get_exp(exp_file=None, exp_name=None):
     """
     get Exp object by file or name. If exp_file and exp_name
     are both provided, get Exp by exp_file.
