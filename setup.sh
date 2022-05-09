369f7ea61459ab3fa20754bcf903d3318ab0013c
Manu Mathew <a0393608@uda0393608>
2022-05-09 11:13:25 +0530

    cuda version to 11.3

--- setup.sh
+++ setup.sh
@@ -3,22 +3,22 @@
 # Dependencies for building pillow-simd
 echo 'Installing dependencies to build pillow-simd'
 echo 'If you do not have sudo access, comment the below line and replace pillow-simd with pillow in the requirements file'
-sudo apt-get install libjpeg-dev zlib1g-dev
+sudo apt-get install -y libjpeg-dev zlib1g-dev
 
 echo "-----------------------------------------------------------"
 echo "installing requirements"
-pip3 install -r requirements.txt
-pip3 install -r references/requirements.txt
+pip3 install --no-input -r requirements.txt
+pip3 install --no-input -r references/requirements.txt
 
 echo "-----------------------------------------------------------"
 echo "building torchvision"
 # may need pytorch nightly to build this package
 echo "installing pytorch for cuda 11.3"
 echo "other versions can be found here: https://pytorch.org/get-started/locally/"
-pip3 install torch==1.11.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install --no-input torch==1.11.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
 
-pip3 install onnx==1.8.1
-pip3 install torchinfo
+pip3 install --no-input onnx==1.8.1
+pip3 install --no-input torchinfo
 
 echo "if your CUDA version has changed, please remove 'build' folder before attempting this installatio."
 echo "otherwise there can be errors while using torchvision c++ ops."
