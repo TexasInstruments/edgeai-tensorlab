cfff084fd256a5c5bd0e69d9ef79c6b89c5da2b5
Manu Mathew <a0393608@uda0393608>
2022-07-28 14:10:19 +0530

    annotation_prefix is now flexible for modelmaker datasets

--- jai_benchmark/datasets/modelmaker_datasets.py
+++ jai_benchmark/datasets/modelmaker_datasets.py
@@ -34,26 +34,34 @@ from . import coco_det
 
 
 class ModelMakerDetectionDataset(coco_det.COCODetection):
-    def __init__(self, num_classes=90, download=False, num_frames=None, name='modelmaker', **kwargs):
+    def __init__(self, num_classes=90, download=False, num_frames=None, name='modelmaker',
+                 annotation_prefix='instances', **kwargs):
         assert 'path' in kwargs and 'split' in kwargs, 'kwargs must have path and split'
         path = kwargs['path']
         split = kwargs['split']
         image_dir = os.path.join(path, split)
-        annotation_file = os.path.join(path, 'annotations', f'instances_{split}.json')
+        annotation_file = os.path.join(path, 'annotations', f'{annotation_prefix}_{split}.json')
         super().__init__(num_classes=num_classes, image_dir=image_dir, annotation_file=annotation_file,
                          download=False, num_frames=num_frames, name=name, **kwargs)
+        with open(annotation_file) as afp:
+            self.dataset_store = json.load(afp)
+        #
 
     def download(self, path, split):
         return
 
+    def get_dataset_info(self):
+        return self.dataset_store
+
 
 class ModelMakerClassificationDataset(DatasetBase):
-    def __init__(self, num_classes=None, download=False, num_frames=None, name='modelmaker', **kwargs):
+    def __init__(self, num_classes=None, download=False, num_frames=None, name='modelmaker',
+                 annotation_prefix='instances', **kwargs):
         assert 'path' in kwargs and 'split' in kwargs, 'kwargs must have path and split'
         path = kwargs['path']
         split = kwargs['split']
         self.image_dir = os.path.join(path, split)
-        self.annotation_file = os.path.join(path, 'annotations', f'labels_{split}.json')
+        self.annotation_file = os.path.join(path, 'annotations', f'{annotation_prefix}_{split}.json')
         with open(self.annotation_file) as afp:
             self.dataset_store = json.load(afp)
         #
@@ -76,6 +84,9 @@ class ModelMakerClassificationDataset(DatasetBase):
 
     def download(self, path, split):
         return
+
+    def get_dataset_info(self):
+        return self.dataset_store
 
     def __getitem__(self, idx, with_label=False, **kwargs):
         image_info = self.images_info[idx]
