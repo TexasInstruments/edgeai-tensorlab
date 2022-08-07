6e0a38dbc65891d36add836e8de424e09ec24e76
Debapriya Maji <a0132471@udtensorlab6>
2022-08-07 18:02:03 +0530

    Added framework for: 1. YCB to COCO conversion 2. YCB dataset loader.

---
+++ tools/ycb2coco.py
@@ -0,0 +1,155 @@
+import os
+import mmcv
+import yaml
+import shutil
+from tqdm import tqdm
+import json
+import argparse
+
+parser = argparse.ArgumentParser("LINEMOD_PBR2COCO_PARSER")
+parser.add_argument("--syn", default=False, action="store_true", help="Use only snthetic data")
+parser.add_argument("--keyframes", default=None,type=str, help="path to the keyframes file list")
+parser.add_argument("--split", default='train', type=str, help="Use selected frames")
+parser.add_argument("--lm", default=False, action="store_true", help="Select only LM classes")
+parser.add_argument("--lmo", default=False, action="store_true", help="Select only LMO classes ")
+parser.add_argument("--lmob", default=False, action="store_true", help="Select only LMO classes including benchvise")
+parser.add_argument("--merge_lm", default=False, action="store_true", help="Merge PBR with LINEMOD dataset")
+parser.add_argument("--merge_lmo", default=False, action="store_true", help="Merge PBR with LINEMOD_Occlusion dataset")
+
+
+args = parser.parse_args()
+
+class_to_name = {
+    0: "002_master_chef_can" , 1: "003_cracker_box" ,  2: "004_sugar_box" , 3: "005_tomato_soup_can",  4: "006_mustard_bottle",
+    5: "007_tuna_fish_can",  6: "008_pudding_box" , 7: "009_gelatin_box", 8: "010_potted_meat_can",  9: "011_banana",
+    10: "019_pitcher_base", 11: "021_bleach_cleanser",  12: "024_bowl", 13: "025_mug", 14: "035_power_drill",
+    15: "036_wood_block", 16: "037_scissors", 17: "040_large_marker", 18: "051_large_clamp", 19: "052_extra_large_clamp",
+    20: "061_foam_brick"
+}
+
+def convert_to_coco_json(merge=False):
+    basepath = '/data/ssd/6d_pose/ycb/{}_real'.format(args.split)
+    data_folders = sorted(os.listdir(basepath))
+
+    outfile = '/data/ssd/6d_pose/ycb/annotations/instances_{}.json'.format(args.split)
+    if args.split == 'test':
+        assert args.keyframes is not None, "keyframe file has to be specified"
+        with open(args.keyframes):
+            keyframes_list = list(open(args.keyframes))
+        keyframes_list = ["00" + keyframe.rstrip()+".png" for keyframe in keyframes_list]
+
+    for data_folder_idx, data_folder in enumerate(data_folders):
+        data_path = os.path.join(basepath, data_folder)
+        annotations_gt = dict()
+        for f in ('scene_gt_info.json', 'scene_gt.json'):
+            path = os.path.join(data_path,  f)
+            if os.path.exists(path):
+                print("Loading {}".format(path))
+                with open(path) as foo:
+                    annotations_gt[f.split('.')[0]] = json.load(foo)
+
+        if not merge or data_folder_idx==0:
+            coco_train = dict()
+            coco_train["images"] = []
+            coco_train["type"] = "instance"
+            coco_train["categories"] = []
+            coco_train["annotations"] = []
+
+            for obj_class in class_to_name:
+                category = dict([
+                    ("supercategory", "object"),
+                    ("id", obj_class),
+                    ("name", class_to_name[obj_class])
+                ])
+
+                coco_train["categories"].append(category)
+
+            obj_count = 0
+
+        pbar = tqdm(enumerate(zip(list(annotations_gt['scene_gt'].items()), list(annotations_gt['scene_gt_info'].items()))), total=len(annotations_gt['scene_gt_info']))
+        num_images = len(list(annotations_gt['scene_gt'].items()))
+        for image_index, objects in pbar:
+            objects_gt, objects_gt_info = objects[0], objects[1]
+            filename = "{:06}".format(image_index+1) + '.png'
+            if args.keyframes is not None and args.split is not 'train':
+                print(os.path.join(data_folder, filename))
+                if os.path.join(data_folder, filename) not in keyframes_list :
+                    continue
+            elif image_index%10 != 0:
+                continue
+
+            height, width = mmcv.imread(data_path + '/rgb/' + filename).shape[:2]
+            image = dict([
+                ("image_folder", data_folder),
+                ("id", image_index+num_images*data_folder_idx), #
+                ("file_name", filename),
+                ("height", height),
+                ("width", width),
+                ("type", 'synthetic') if 'real' not in path else ("type", 'real')
+            ])
+            coco_train["images"].append(image)
+            for object_gt, object_gt_info  in zip(objects_gt[1], objects_gt_info[1]):
+                if object_gt_info['visib_fract'] > 0:
+                    annotation = dict([
+                        ("image_id", image_index+num_images*data_folder_idx),
+                        ("id", obj_count),
+                        ("bbox", object_gt_info["bbox_obj"]),
+                        ("area", object_gt_info["bbox_obj"][2] * object_gt_info["bbox_obj"][3]),
+                        ("iscrowd", 0),
+                        ("category_id", object_gt["obj_id"]-1),
+                        ("R", object_gt["cam_R_m2c"]),
+                        ("T", object_gt["cam_t_m2c"])
+                    ])
+                    obj_count += 1
+                    coco_train["annotations"].append(annotation)
+        pbar.close()
+
+    #Merge either with the LINEMOD or LINEMOD Occlusion dataset.
+    if args.merge_lm:
+        lm_real_path = "/data/ssd/6d_pose/LINEMOD_Occlusion_COCO_pbr/annotations/lm_real_merged/08_classes/instances_train.json"
+        with open(lm_real_path) as foo:
+            lm_real_gt = json.load(foo)
+            print("loading real LINEMOD Occlusion gt data")
+            for image_dict in lm_real_gt["images"]:
+                image_dict['id'] = image_dict['id'] + num_images * 50
+                print("image_dict", image_dict['id'])
+
+            for annotation_dict in lm_real_gt["annotations"]:
+                annotation_dict['image_id'] = annotation_dict['image_id'] + num_images*50 # num_folders=50. TO remove the hard-coding
+                annotation_dict['id'] = annotation_dict['id'] + obj_count
+                print("annotation_dict", annotation_dict['image_id'])
+
+            coco_train["annotations"].extend(lm_real_gt["annotations"])
+            coco_train["images"].extend(lm_real_gt["images"])
+
+    elif args.merge_lmo:
+        lmo_real_path = "/data/ssd/6d_pose/LINEMOD_Occlusion_COCO/annotations/instances_train.json"
+        with open(lmo_real_path) as foo:
+            lm_real_gt = json.load(foo)
+            print("loading real LINEMOD Occlusion gt data")
+            for image_dict in lm_real_gt["images"]:
+                image_dict['id'] = image_dict['id'] + num_images * 50
+                print("image_dict", image_dict['id'])
+
+            for annotation_dict in lm_real_gt["annotations"]:
+                annotation_dict['image_id'] = annotation_dict['image_id'] + num_images*50 # num_folders=50. TO remove the hard-coding
+                annotation_dict['id'] = annotation_dict['id'] + obj_count
+                print("annotation_dict", annotation_dict['image_id'])
+
+            coco_train["annotations"].extend(lm_real_gt["annotations"])
+            coco_train["images"].extend(lm_real_gt["images"])
+
+
+    mmcv.dump(coco_train, outfile)
+
+def sort_images(src, train_dst, test_dst, test_list):
+    for image_num in range(1214):
+        filename = "{:04}".format(image_num) + '.png'
+        if filename[:-4] in test_list:
+            shutil.copy(os.path.join(src,filename), os.path.join(test_dst, filename))
+        else:
+            shutil.copy(os.path.join(src,filename), os.path.join(train_dst, filename))
+
+
+if __name__ == "__main__":
+        convert_to_coco_json(merge=True)
---
+++ yolox/data/datasets/ycb.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+from loguru import logger
+
+import cv2
+import numpy as np
+from math import acos, sin, sqrt, copysign
+from pycocotools.coco import COCO
+from plyfile import PlyData
+import yaml
+
+from ..dataloading import get_yolox_datadir
+from .datasets_wrapper import Dataset
+from yolox.utils import camera_matrix
+from .linemod_occlusion import CADModels
+
+class YCBDataset(Dataset):
+    """
+    LINEMODOcclusion dataset class.
+    """
+
+    def __init__(
+        self,
+        data_dir=None,
+        json_file="instances_train.json",
+        name="train",
+        img_size=(416, 416),
+        preproc=None,
+        cache=False,
+        object_pose=False,
+        symmetric_objects= { 1 : "002_master_chef_can" , 13 : "024_bowl" , 14 : "025_mug", 16 : "036_wood_block",
+                            18 : "040_large_marker", 19 : "051_large_clamp", 20 : "052_extra_large_clamp", 21 : "061_foam_brick"}
+    ):
+        """
+        LINEMODOcclusion dataset initialization. Annotation data are read into memory by COCO API.
+        Args:
+            data_dir (str): dataset root directory
+            json_file (str): LINEMOD Occlusion json file name
+            name (str): LINEMOD Occlusion data name 
+            img_size (int): target image size after pre-processing
+            preproc: data augmentation strategy
+        """
+        super().__init__(img_size)
+        if data_dir is None:
+            data_dir = os.path.join(get_yolox_datadir(), "ycb")
+        self.data_dir = data_dir
+        self.json_file = json_file
+        self.object_pose = object_pose 
+
+        self.coco = COCO(os.path.join(self.data_dir, "annotations", self.json_file))
+        self.ids = self.coco.getImgIds()
+        self.class_ids = sorted(self.coco.getCatIds())
+        cats = self.coco.loadCats(self.coco.getCatIds())
+        self._classes = tuple([c["name"] for c in cats])
+        self.imgs = None
+        self.imgs_coco = self.coco.imgs
+        self.name = name
+        self.img_size = img_size
+        if preproc is not None:
+            self.preproc = preproc
+        self.annotations = self._load_coco_annotations()
+        self.cad_models = CADModels()
+        self.models_corners, self.models_diameter = self.cad_models.models_corners, self.cad_models.models_diameter
+        self.class_to_name = self.cad_models.class_to_name
+        self.class_to_model = self.cad_models.class_to_model
+        self.symmetric_objects = symmetric_objects
+        if cache:
+            self._cache_images()
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __del__(self):
+        del self.imgs
+
+    def _load_coco_annotations(self):
+        return [self.load_anno_from_ids(_ids) for _ids in self.ids]
+
+    def _cache_images(self):
+        logger.warning(
+            "\n********************************************************************************\n"
+            "You are using cached images in RAM to accelerate training.\n"
+            "This requires large system RAM.\n"
+            "Make sure you have 200G+ RAM and 136G available disk space for training COCO.\n"
+            "********************************************************************************\n"
+        )
+        max_h = self.img_size[0]
+        max_w = self.img_size[1]
+        cache_file = self.data_dir + "/img_resized_cache_" + self.name + ".array"
+        if not os.path.exists(cache_file):
+            logger.info(
+                "Caching images for the first time. This might take about 20 minutes for COCO"
+            )
+            self.imgs = np.memmap(
+                cache_file,
+                shape=(len(self.ids), max_h, max_w, 3),
+                dtype=np.uint8,
+                mode="w+",
+            )
+            from tqdm import tqdm
+            from multiprocessing.pool import ThreadPool
+
+            NUM_THREADs = min(8, os.cpu_count())
+            loaded_images = ThreadPool(NUM_THREADs).imap(
+                lambda x: self.load_resized_img(x),
+                range(len(self.annotations)),
+            )
+            pbar = tqdm(enumerate(loaded_images), total=len(self.annotations))
+            for k, out in pbar:
+                self.imgs[k][: out.shape[0], : out.shape[1], :] = out.copy()
+            self.imgs.flush()
+            pbar.close()
+        else:
+            logger.warning(
+                "You are using cached imgs! Make sure your dataset is not changed!!"
+            )
+
+        logger.info("Loading cached imgs...")
+        self.imgs = np.memmap(
+            cache_file,
+            shape=(len(self.ids), max_h, max_w, 3),
+            dtype=np.uint8,
+            mode="r+",
+        )
+
+    def load_anno_from_ids(self, id_):
+        im_ann = self.coco.loadImgs(id_)[0]
+        width = im_ann["width"]
+        height = im_ann["height"]
+        anno_ids = self.coco.getAnnIds(imgIds=[int(id_)], iscrowd=False)
+        annotations = self.coco.loadAnns(anno_ids)
+        objs = []
+        for obj in annotations:
+            x1 = np.max((0, obj["bbox"][0]))
+            y1 = np.max((0, obj["bbox"][1]))
+            x2 = np.min((width, x1 + np.max((0, obj["bbox"][2]))))
+            y2 = np.min((height, y1 + np.max((0, obj["bbox"][3]))))
+            if obj["area"] > 0 and x2 >= x1 and y2 >= y1:
+                obj["clean_bbox"] = [x1, y1, x2, y2]
+                objs.append(obj)
+
+        num_objs = len(objs)
+
+        if self.object_pose:
+            res = np.zeros((num_objs, 14))
+        else:
+            res = np.zeros((num_objs, 5))
+
+        for ix, obj in enumerate(objs):
+            cls = self.class_ids.index(obj["category_id"])
+            res[ix, 0:4] = obj["clean_bbox"]
+            res[ix, 4] = cls
+            #Convert the rotation matrix to angle axis format using Rodrigues formula
+            #https://www.ccs.neu.edu/home/rplatt/cs5335_fall2017/slides/euler_quaternions.pdf
+            if self.object_pose:
+                temp_R, _ = cv2.Rodrigues(np.array(obj["R"]).reshape(3,3))
+                temp_R = np.squeeze(temp_R)
+
+                obj_centre_2d = np.matmul(camera_matrix.reshape(3,3), np.array(obj["T"])/obj["T"][2])[:2]  #rotation vec not required for the center point
+                #res[ix, 11:14] = obj["T"]
+                obj_centre_2d = np.squeeze(obj_centre_2d)
+                res[ix, 11:13] = obj_centre_2d
+                res[ix, 13] = obj["T"][2] / 100.0
+                #obj["R_aa"], _ = cv2.Rodrigues(np.array(obj["R"]).reshape(3,3))
+                #obj["R_aa"] = np.squeeze(obj["R_aa"])
+                #Use Gram-Schmidt to make the rotation representation continuous and in 6D
+                #https://towardsdatascience.com/better-rotation-representations-for-accurate-pose-estimation-e890a7e1317f
+                R_gs = np.array(obj["R"]).reshape(3,3)
+                obj["R_gs"] = np.squeeze(R_gs[:, :2].transpose().reshape(6, 1))
+                res[ix, 5:11] = obj["R_gs"]
+            #print(res[ix, 11:13])
+        r = min(self.img_size[0] / height, self.img_size[1] / width)
+        res[:, :4] *= r
+        res[:, 11:13] *= r
+
+        img_info = (height, width)
+        resized_info = (int(height * r), int(width * r))
+
+        file_name = (
+            im_ann["file_name"]
+            if "file_name" in im_ann
+            else "{:04}".format(id_) + ".png"
+        )
+
+        return (res, img_info, resized_info, file_name)
+
+    def load_anno(self, index):
+        return self.annotations[index][0]
+
+    def load_resized_img(self, index):
+        img = self.load_image(index)
+        r = min(self.img_size[0] / img.shape[0], self.img_size[1] / img.shape[1])
+        resized_img = cv2.resize(
+            img,
+            (int(img.shape[1] * r), int(img.shape[0] * r)),
+            interpolation=cv2.INTER_LINEAR,
+        ).astype(np.uint8)
+        return resized_img
+
+    def load_image(self, index):
+        file_name = self.annotations[index][3]
+        img_index = list(self.imgs_coco)[index]
+        if self.name=='train' and self.imgs_coco[img_index]['type'] == 'synthetic':
+            image_folder = self.imgs_coco[int(index)]['image_folder']
+
+            img_file = os.path.join(self.data_dir, self.name+"_pbr", image_folder, 'rgb', file_name)
+        else:
+            img_file = os.path.join(self.data_dir, self.name, file_name)
+
+        img = cv2.imread(img_file)
+        assert img is not None
+
+        return img
+
+    def pull_item(self, index):
+        id_ = self.ids[index]
+
+        res, img_info, resized_info, _ = self.annotations[index]
+        if self.imgs is not None:
+            pad_img = self.imgs[index]
+            img = pad_img[: resized_info[0], : resized_info[1], :].copy()
+        else:
+            img = self.load_resized_img(index)
+
+        return img, res.copy(), img_info, np.array([id_])
+
+    @Dataset.mosaic_getitem
+    def __getitem__(self, index):
+        """
+        One image / label pair for the given index is picked up and pre-processed.
+
+        Args:
+            index (int): data index
+
+        Returns:
+            img (numpy.ndarray): pre-processed image
+            padded_labels (torch.Tensor): pre-processed label data.
+                The shape is :math:`[max_labels, 5]`.
+                each label consists of [class, xc, yc, w, h]:
+                    class (float): class index.
+                    xc, yc (float) : center of bbox whose values range from 0 to 1.
+                    w, h (float) : size of bbox whose values range from 0 to 1.
+            info_img : tuple of h, w.
+                h, w (int): original shape of the image
+            img_id (int): same as the input index. Used for evaluation.
+        """
+        img, target, img_info, img_id = self.pull_item(index)
+
+        if self.preproc is not None:
+            img, target = self.preproc(img, target, self.input_dim)
+        return img, target, img_info, img_id
