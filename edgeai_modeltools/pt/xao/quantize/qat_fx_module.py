3697ca015ac7809d5bb273f3aeec69cba0f5e3cf
Manu Mathew <a0393608@uda0393608>
2022-08-09 00:02:18 +0530

    qat scripts update

--- edgeai_modeltools/pt/xao/quantize/qat_fx_module.py
+++ edgeai_modeltools/pt/xao/quantize/qat_fx_module.py
@@ -1,17 +1,23 @@
 import copy
 import torch
 import torch.quantization.quantize_fx as quantize_fx
+from ... import xnn
 
 
 class QATFxModule(torch.nn.Module):
-    def __init__(self, module, qconfig_dict=None):
+    def __init__(self, module, qconfig_dict=None, pretrained=None, pretrained_after=True):
         super().__init__()
         if qconfig_dict is None:
             qconfig_dict = {"": torch.quantization.get_default_qat_qconfig('qnnpack')}
         #
         module.train()
+        if not pretrained_after:
+            self.load_weights_(module, pretrained=pretrained)
+        #
         module = quantize_fx.prepare_qat_fx(module, qconfig_dict)
         self.module = module
+        if pretrained_after:
+            self.load_weights_(module, pretrained=pretrained)
 
     def forward(self, *args, **kwargs):
         return self.module(*args, **kwargs)
@@ -20,3 +26,12 @@ class QATFxModule(torch.nn.Module):
         self.module = quantize_fx.convert_fx(self.module)
         return self
 
+    def load_weights_(self, module, pretrained=None, change_names_dict=None):
+        # Load weights for accuracy evaluation of a QAT model
+        if pretrained is not None and pretrained is not False:
+            print("=> using pre-trained model from {}".format(pretrained))
+            if hasattr(module, 'load_weights'):
+                module.load_weights(args.pretrained, download_root='./data/downloads', change_names_dict=change_names_dict)
+            else:
+                xnn.utils.load_weights(module, pretrained, download_root='./data/downloads', change_names_dict=change_names_dict)
+            #
