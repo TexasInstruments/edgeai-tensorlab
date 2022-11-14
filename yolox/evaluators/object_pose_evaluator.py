1. Bug fixes for ADD(S) calculation. 2. Changed visualization from separate image to a single imae for gt and pred.

yolox/evaluators/object_pose_evaluator.py
@@ -144,10 +144,11 @@ class ObjectPoseEvaluator:
                     for output_idx in range(len(predicted_pose)):
                         img = imgs[output_idx]
                         if len(frame_data_list) != 0:
-                            visualize_object_pose.draw_6d_pose(img, frame_data_list, camera_matrix, class_to_model=self.class_to_model,
+                            img_cuboid, img_mask, img_2dod = visualize_object_pose.draw_6d_pose(img, frame_data_list, camera_matrix, class_to_model=self.class_to_model,
                                                                         class_to_cuboid=self.class_to_cuboid, out_dir=self.output_dir, id=ids[output_idx])
                             visualize_object_pose.draw_6d_pose(img, frame_data_list, camera_matrix, class_to_model=self.class_to_model,
-                                                                    class_to_cuboid=self.class_to_cuboid, gt=False, out_dir=self.output_dir, id=ids[output_idx])
+                                                                    class_to_cuboid=self.class_to_cuboid, gt=False, out_dir=self.output_dir, id=ids[output_idx],
+                                                                    img_cuboid=img_cuboid, img_mask=img_mask, img_2dod=img_2dod)
                     if self.test_bop:
                         csv_file_path = os.path.join(self.output_dir, self.test_bop_file_name)
                         with open(csv_file_path, 'a') as csv_file:
@@ -321,15 +322,11 @@ class ObjectPoseEvaluator:
             return 0, 0
         data_dict_asym = []
         data_dict_sym = []
-        missing_det_per_class = {}
         for pred_data in data_dict:
-            if not pred_data['missing_det']:
-                if self.class_to_name[pred_data['category_id']] not in self.symmetric_objects.values():
-                    data_dict_asym.extend([pred_data])
-                else:
-                    data_dict_sym.extend([pred_data])
+            if self.class_to_name[pred_data['category_id']] not in self.symmetric_objects.values():
+                data_dict_asym.extend([pred_data])
             else:
-                pass   #ToDo: Keep track of the missing detections for each class.
+                data_dict_sym.extend([pred_data])
         score_dict_asym = self.compute_add_score(data_dict_asym)
         score_dict_sym = self.compute_adds_score(data_dict_sym)
         score_dict = {}
@@ -350,19 +347,23 @@ class ObjectPoseEvaluator:
 
 
     def compute_add_score(self, data_dict, percentage=0.1):
-        distance_category = np.zeros((len(data_dict), 2))
+        distance_category = np.zeros((len(data_dict), 2))   #stores distance and category for each object in a row
         for index, pred_data in enumerate(data_dict):
-            R_gt, t_gt = np.array(pred_data['rotation_gt']), np.array(pred_data['translation_gt'])
-            R_gt, _ = cv2.Rodrigues(R_gt)
-            R_pred, t_pred= np.array(pred_data['rotation_pred']), np.array(pred_data['translation_pred'])
-            R_pred, _ = cv2.Rodrigues(R_pred)
-            pts3d = self.class_to_model[pred_data['category_id']]
-            #mean_distances = np.zeros((count,), dtype=np.float32)
-            pts_xformed_gt = np.matmul(R_gt,  pts3d.transpose()) + t_gt[:, None]
-            pts_xformed_pred = np.matmul(R_pred, pts3d.transpose()) + t_pred[:, None]
-            distance = np.linalg.norm(pts_xformed_gt - pts_xformed_pred, axis=0)
-            distance_category[index, 0] = np.mean(distance)
-            distance_category[index, 1] = pred_data['category_id']
+            if not pred_data['missing_det']:
+                R_gt, t_gt = np.array(pred_data['rotation_gt']), np.array(pred_data['translation_gt'])
+                R_gt, _ = cv2.Rodrigues(R_gt)
+                R_pred, t_pred= np.array(pred_data['rotation_pred']), np.array(pred_data['translation_pred'])
+                R_pred, _ = cv2.Rodrigues(R_pred)
+                pts3d = self.class_to_model[pred_data['category_id']]
+                #mean_distances = np.zeros((count,), dtype=np.float32)
+                pts_xformed_gt = np.matmul(R_gt,  pts3d.transpose()) + t_gt[:, None]
+                pts_xformed_pred = np.matmul(R_pred, pts3d.transpose()) + t_pred[:, None]
+                distance = np.linalg.norm(pts_xformed_gt - pts_xformed_pred, axis=0)
+                distance_category[index, 0] = np.mean(distance)
+                distance_category[index, 1] = pred_data['category_id']
+            else:
+                distance_category[index, 0] = 1e6 # This distance must be set to a very high value for missign detections.
+                distance_category[index, 1] = pred_data['category_id']
 
         threshold = [self.class_to_diameter[category] * percentage for category in self.class_to_diameter.keys()]
         score_dict = {}
@@ -380,19 +381,24 @@ class ObjectPoseEvaluator:
     def compute_adds_score(self, data_dict, percentage=0.1):
         distance_category = np.zeros((len(data_dict), 2))
         for index, pred_data in enumerate(data_dict):
-            R_gt, t_gt = np.array(pred_data['rotation_gt']), np.array(pred_data['translation_gt'])
-            R_gt, _ = cv2.Rodrigues(R_gt)
-            R_pred, t_pred = np.array(pred_data['rotation_pred']), np.array(pred_data['translation_pred'])
-            R_pred, _ = cv2.Rodrigues(R_pred)
-            pts3d = self.class_to_model[pred_data['category_id']]
-            pts_xformed_gt = np.matmul(R_gt, pts3d.transpose()) + t_gt[:, None]
-            pts_xformed_pred = np.matmul(R_pred, pts3d.transpose()) + t_pred[:, None]
-            kdt = KDTree(pts_xformed_gt.transpose(), metric='euclidean')
-            distance, _ = kdt.query(pts_xformed_pred.transpose(), k=1)
-            # distance_np = np.sqrt(     #brute-force distance calculation
-            #     np.min(np.sum((pts_xformed_gt[:, :, None] - pts_xformed_pred[:, None, :]) ** 2, axis=0), axis=1))
-            distance_category[index, 0] = np.mean(distance)
-            distance_category[index, 1] = pred_data['category_id']
+            if not pred_data['missing_det']:
+                R_gt, t_gt = np.array(pred_data['rotation_gt']), np.array(pred_data['translation_gt'])
+                R_gt, _ = cv2.Rodrigues(R_gt)
+                R_pred, t_pred = np.array(pred_data['rotation_pred']), np.array(pred_data['translation_pred'])
+                R_pred, _ = cv2.Rodrigues(R_pred)
+                pts3d = self.class_to_model[pred_data['category_id']]
+                pts_xformed_gt = np.matmul(R_gt, pts3d.transpose()) + t_gt[:, None]
+                pts_xformed_pred = np.matmul(R_pred, pts3d.transpose()) + t_pred[:, None]
+                kdt = KDTree(pts_xformed_gt.transpose(), metric='euclidean')
+                distance, _ = kdt.query(pts_xformed_pred.transpose(), k=1)
+                # distance_np = np.sqrt(     #brute-force distance calculation
+                #     np.min(np.sum((pts_xformed_gt[:, :, None] - pts_xformed_pred[:, None, :]) ** 2, axis=0), axis=1))
+                distance_category[index, 0] = np.mean(distance)
+                distance_category[index, 1] = pred_data['category_id']
+            else:
+                distance_category[index, 0] = 1e6 # This must be set to a very high value
+                distance_category[index, 1] = pred_data['category_id']
+
 
         threshold = [self.class_to_diameter[category] * percentage for category in self.class_to_diameter.keys()]
         score_dict = {}

yolox/utils/visualize_object_pose.py
@@ -31,9 +31,16 @@ def draw_bbox_2d(img, box, label, score, conf = 0.6, thickness=1, gt=False):
     x0, y0 = int(box[0]), int(box[1])
     x1, y1 = int(box[0] + box[2]), int(box[1] + box[3])
 
-    color = colors(cls_id)
+    if gt:
+        color = (0, 255, 0)
+    else:
+        color = colors(cls_id)
+
     cv2.rectangle(img, (x0, y0), (x1, y1), color, thickness)
-    cv2.putText(img, str(label), (x0, y0), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=thickness)
+    if gt:
+        cv2.putText(img, str(label), ((x0+x1)//2, (y0+y1)//2), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=thickness)
+    else:
+        cv2.putText(img, str(label), (x0, y0), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=thickness)
     return img
 
 
@@ -73,18 +80,19 @@ def project_3d_2d(pts_3d, rotation_vec, translation_vec, camera_matrix):
     return projected_2d
 
 
-def draw_6d_pose(img, data_list , camera_matrix, class_to_cuboid=None, conf = 0.6, class_to_model=None, gt=True, out_dir=None, id=None):
+def draw_6d_pose(img, data_list , camera_matrix, class_to_cuboid=None, conf = 0.6, class_to_model=None, gt=True, out_dir=None, id=None, img_cuboid=None, img_mask=None, img_2dod=None):
 
     if is_tensor(img):
-        img_cuboid = copy.deepcopy(img).cpu().numpy().transpose(1, 2, 0)
-        img_cuboid = np.asarray(img_cuboid, dtype=np.uint8)
-        img_cuboid = np.ascontiguousarray(img_cuboid)
-        img_mask = copy.deepcopy(img_cuboid)
-        img_2dod = copy.deepcopy(img_cuboid)
+        img_temp = copy.deepcopy(img).cpu().numpy().transpose(1, 2, 0)
+        img_temp = np.asarray(img_temp, dtype=np.uint8)
+        img_temp = np.ascontiguousarray(img_temp)
+        if img_mask is None:
+            img_mask = copy.deepcopy(img_temp)
+        if img_cuboid is None:
+            img_cuboid = copy.deepcopy(img_temp)
+        if img_2dod is None:
+            img_2dod = copy.deepcopy(img_temp)
 
-    # if is_tensor(poses):
-    #     poses = poses.cpu().numpy()
-   
     for pose in data_list:
         #Rotation matrix is recovered using the formula given in the article
         #https://towardsdatascience.com/better-rotation-representations-for-accurate-pose-estimation-e890a7e1317f
@@ -95,15 +103,19 @@ def draw_6d_pose(img, data_list , camera_matrix, class_to_cuboid=None, conf = 0.
         if score < conf:
             continue
         rotation, translation, bbox, xy,  label = \
-            np.array(pose['rotation_{}'.format(pose_type)]), np.array(pose['translation_{}'.format(pose_type)]), pose['bbox_{}'.format(pose_type)], pose['xy_{}'.format(pose_type)], pose['category_id']
-        colour = colors(label)
+            np.array(pose['rotation_{}'.format(pose_type)]), np.array(pose['translation_{}'.format(pose_type)]), \
+                        pose['bbox_{}'.format(pose_type)], pose['xy_{}'.format(pose_type)], pose['category_id']
+        if gt:
+            colour = (0, 255, 0)
+        else:
+            colour = colors(label)
 
         img_cuboid = cv2.circle(img_cuboid, (int(xy[0]), int(xy[1])), 3, (0, 0, 255), -1)
 
         cad_model_2d = project_3d_2d(class_to_model[label], rotation, translation, camera_matrix)
         cad_model_2d = cad_model_2d.astype(np.int32)
-        cad_model_2d[:, 0][cad_model_2d[:, 0] >= img.shape[1]] = img.shape[1]
-        cad_model_2d[:, 1][cad_model_2d[:, 1] >= img.shape[0]] = img.shape[0]
+        cad_model_2d[:, 0][cad_model_2d[:, 0] >= img.shape[2]] = img.shape[2] - 1
+        cad_model_2d[:, 1][cad_model_2d[:, 1] >= img.shape[1]] = img.shape[1] - 1
         cad_model_2d[cad_model_2d < 0] = 0
         img_mask[cad_model_2d[:, 1], cad_model_2d[:, 0]] = colour
         img_mask = cv2.circle(img_mask, (int(xy[0]), int(xy[1])), 3, (0, 0, 255), -1)
@@ -116,7 +128,9 @@ def draw_6d_pose(img, data_list , camera_matrix, class_to_cuboid=None, conf = 0.
     outfile_pose = os.path.join(out_dir, "vis_pose", "{:012}_{}_pose.png".format(id, pose_type))
     outfile_mask = os.path.join(out_dir, "vis_pose", "{:012}_{}_mask.png".format(id, pose_type))
     outfile_2d_od = os.path.join(out_dir, "vis_pose", "{:012}_{}_2d_od.png".format(id, pose_type))
-    cv2.imwrite(outfile_pose, img_cuboid)
-    cv2.imwrite(outfile_mask, img_mask)
-    cv2.imwrite(outfile_2d_od, img_2dod)
+    if not gt:
+        cv2.imwrite(outfile_pose, img_cuboid)
+        cv2.imwrite(outfile_mask, img_mask)
+        cv2.imwrite(outfile_2d_od, img_2dod)
+    return img_cuboid, img_mask, img_2dod
 