9261401ff76e531fcc23020b74810956959ed9e3
Debapriya Maji <d-maji@ti.com>
2022-12-08 23:17:06 +0530

    Modified config for op-7200.

--- configs/object_6d_pose_estimation.py
+++ configs/object_6d_pose_estimation.py
@@ -61,7 +61,7 @@ def get_configs(settings, work_dir):
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_p2(),
                         {
                          'tensor_bits': 16,
-                         'advanced_options:calibration_iterations': 3,
+                         'advanced_options:calibration_iterations': 2,
                          'object_detection:meta_arch_type': 6,
                          'object_detection:meta_layers_names_list': f'{settings.models_path}/vision/object_6d_pose/ycbv/edgeai-yolox/yolox_s_object_pose_ti_lite_metaarch_640x480.prototxt',
                         'advanced_options:output_feature_16bit_names_list': '597, 837, 840, 843, 847, 850, 853, 856, 1098, 1101, 1104, 1108, 1111, 1114, 1117, 1359, 1362, 1365, 1369, 1372, 1375, 1378',
@@ -69,7 +69,7 @@ def get_configs(settings, work_dir):
                         }),
                 model_path=f'{settings.models_path}/vision/object_6d_pose/ycbv/edgeai-yolox/yolox_s_object_pose_ti_lite_640x480_57p75.onnx'),
             postprocess=postproc_transforms.get_transform_detection_yolo_6d_object_pose_onnx(squeeze_axis=None, normalized_detections=False, resize_with_pad=True, formatter=postprocess.DetectionBoxSL2BoxLS(), object6dpose=True),
-            model_info=dict(metric_reference={'accuracy_add(s)_p1%':57.75}, model_shortlist=15)
+            model_info=dict(metric_reference={'accuracy_add(s)_p1%':54.10}, model_shortlist=15)
         ),
     }
     return pipeline_configs
