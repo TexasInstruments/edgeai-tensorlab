6a17377a31cde95e52f268f32587a5469aa3d1df
Badrinath Gonnabattula <a0504871@udtensorlab2>
2023-02-17 16:27:02 +0530

    Resolved NaN issue

--- torchvision/edgeailite/xvision/losses/segmentation_loss.py
+++ torchvision/edgeailite/xvision/losses/segmentation_loss.py
@@ -109,6 +109,11 @@ class SegmentationMetricsCalc():
     @staticmethod
     def _fast_hist(label_pred, label_true, n_class):
         mask = (label_true >= 0) & (label_true < n_class)
+        hist = np.zeros((n_class, n_class))
+        label_pred[label_pred>=n_class] = n_class-1
+        label_pred[label_pred < 0] = 0
+        if np.bincount(n_class * label_true[mask].astype(int) + label_pred[mask], minlength=n_class**2).shape[0] != n_class**2:
+            print(np.bincount(n_class * label_true[mask].astype(int) + label_pred[mask], minlength=n_class**2).shape)
         if np.bincount(n_class * label_true[mask].astype(int) + label_pred[mask], minlength=n_class ** 2).shape[0] == n_class**2:
             hist = np.bincount(n_class * label_true[mask].astype(int) + label_pred[mask], minlength=n_class**2).reshape(n_class, n_class)
         return hist
