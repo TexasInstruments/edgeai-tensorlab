1cf98c45eb0bb4b7109b667e4b2d8363f533e91c
Manu Mathew <a0393608@uda0393608>
2023-02-24 07:47:09 +0530

    target_device is now passed from sh script

--- run_custom_pc.sh
+++ run_custom_pc.sh
@@ -55,7 +55,7 @@ settings_file=settings_import_on_pc.yaml
 
 echo "==================================================================="
 ## run all the shortlisted models with these settings
-python3 ./scripts/benchmark_custom.py ${settings_file}
+python3 ./scripts/benchmark_custom.py ${settings_file} --target_device ${TARGET_SOC}
 echo "-------------------------------------------------------------------"
 
 echo "==================================================================="
