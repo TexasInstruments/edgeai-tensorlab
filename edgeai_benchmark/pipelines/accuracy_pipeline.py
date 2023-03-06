5c82306e665ace595506e4c394dc0309fc9fb7f8
Varun Tripathi <v-tripathi@ti.com>
2023-03-06 11:40:25 +0530

    [Logger] Fixing default values for buffering

--- edgeai_benchmark/pipelines/accuracy_pipeline.py
+++ edgeai_benchmark/pipelines/accuracy_pipeline.py
@@ -104,7 +104,7 @@ class AccuracyPipeline():
 
         # start logger - run_dir has been created in start() above
         log_filename = os.path.join(self.run_dir, 'run.log') if self.settings.enable_logging else None
-        logger_buffering = (1 if self.settings.capture_log else None)
+        logger_buffering = (1 if self.settings.capture_log else -1)
         self.logger = utils.TeeLogger(log_filename, append=True, buffering=logger_buffering)
 
         # log some info
