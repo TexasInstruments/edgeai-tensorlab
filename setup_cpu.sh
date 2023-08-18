3f5eec49ed4d2c2161d276408d6b06b58d92583e
Manu Mathew <a0393608@udtensorlab9>
2023-08-18 22:09:25 +0530

    setup files updated

--- setup_cpu.sh
+++ setup_cpu.sh
@@ -1,26 +1,35 @@
-#echo "mmdetection is not installing cython properly - it has to be installed from conda if this is conda python"
-#conda install -y cython
+#!/usr/bin/env bash
+
+# system packages
+sudo apt-get install -y libjpeg-dev zlib1g-dev cmake libffi-dev protobuf-compiler
 
 echo "Installing pytorch"
-pip3 install --no-input torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cpu
+pip3 install --no-input torch==2.0.1+cpu torchvision==0.15.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
-pip3 install --no-input cython
+echo 'Installing python packages...'
+# there as issue with installing pillow-simd through requirements - force it here
+pip uninstall --yes pillow
+pip install --no-input -U --force-reinstall pillow-simd
+pip3 install --no-input cython wheel numpy==1.23.0
+pip3 install --no-input torchinfo pycocotools opencv-python
+
+echo "installing requirements"
+pip3 install --no-input -r requirements.txt
 
 echo "Installing mmcv"
-CUDA_VISIBLE_DEVICES="" pip3 install --no-input mmcv-full==1.4.7 -f https://download.openmmlab.com/mmcv/dist/cpu/torch2.0.1/index.html
+FORCE_CUDA=0 pip3 install --no-input mmcv-full==1.4.8 -f https://download.openmmlab.com/mmcv/dist/cpu/torch2.0.1/index.html
 
-pip3 install --no-input cython
-pip3 install --no-input protobuf==3.19.4
-# onnx
-pip3 install --no-input pybind11[global]         # for building the below onnx custom fork
-pip3 install --no-input https://github.com/TexasInstruments/onnx/archive/tidl-j7.zip
-
-pip3 install --no-input torchinfo
-pip3 install --no-input pycocotools
+# building onnx from soure requires carefull steps
+# make sure that we are using system cmake
+pip uninstall --yes cmake
+# pybind11[global] is needed for building the onnx package.
+# for some reason, this has to be installed before the requirements file is used.
+pip3 install --no-input pybind11[global] protobuf==3.19.4
+pybind11_DIR=$(pybind11-config --cmakedir) pip3 install --no-input https://github.com/TexasInstruments/onnx/archive/tidl-j7.zip
 
 echo "Installing mmdetection"
 echo "For more details, see: https://github.com/open-mmlab/mmdetection/blob/master/docs/get_started.md and https://github.com/open-mmlab/mmdetection"
-CUDA_VISIBLE_DEVICES="" python3 setup.py develop
+FORCE_CUDA=0 python3 setup.py develop
 
 echo "This package depends on edgeai-modeltoolkit. Please clone and install that package."
 echo "URL: https://github.com/TexasInstruments/edgeai-modeltoolkit"
