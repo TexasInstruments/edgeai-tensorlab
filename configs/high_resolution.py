a11645be48a110d0d7b61b9e3296a96d14824df7
Manu Mathew <a0393608@udtensorlab9>
2023-09-12 16:08:28 +0530

    model_shortlist=60 has been added

--- configs/high_resolution.py
+++ configs/high_resolution.py
@@ -73,14 +73,14 @@ def get_configs(settings, work_dir):
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/edgeai-tv/mobilenet_v1_20190906_{hr_input_sizes_x[0]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         'cl-6062':utils.dict_update(common_cfg,
             preprocess=preproc_transforms.get_transform_onnx(resize=hr_input_sizes[1], crop=hr_input_sizes[1]),
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/edgeai-tv/mobilenet_v1_20190906_{hr_input_sizes_x[1]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         # jai-devkit: classification mobilenetv2_224x224 expected_metric: 72.13% top-1 accuracy
         'cl-6091':utils.dict_update(common_cfg,
@@ -103,14 +103,14 @@ def get_configs(settings, work_dir):
             session=onnx_session_type(**sessions.get_onnx_quant_session_cfg(settings, work_dir=work_dir, input_optimization=False),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_p2_qat(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/edgeai-tv/mobilenet_v2_1p4_qat-p2_20210112_{hr_input_sizes_x[0]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         'cl-6152':utils.dict_update(common_cfg,
             preprocess=preproc_transforms.get_transform_onnx(resize=hr_input_sizes[1], crop=hr_input_sizes[1]),
             session=onnx_session_type(**sessions.get_onnx_quant_session_cfg(settings, work_dir=work_dir, input_optimization=False),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_p2_qat(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/edgeai-tv/mobilenet_v2_1p4_qat-p2_20210112_{hr_input_sizes_x[1]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         # torchvision: classification resnet18_224x224 expected_metric: 69.76% top-1 accuracy
         'cl-6101':utils.dict_update(common_cfg,
@@ -133,14 +133,14 @@ def get_configs(settings, work_dir):
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_p2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/resnet50_{hr_input_sizes_x[0]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':76.15}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':76.15}, model_shortlist=60)
         ),
         'cl-6112':utils.dict_update(common_cfg,
             preprocess=preproc_transforms.get_transform_onnx(resize=hr_input_sizes[1], crop=hr_input_sizes[1]),
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_p2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/resnet50_{hr_input_sizes_x[1]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':76.15}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':76.15}, model_shortlist=60)
         ),
         # pycls: classification regnetx400mf_224x224 expected_metric: 72.7% top-1 accuracy
         'cl-6121':utils.dict_update(common_cfg,
@@ -148,14 +148,14 @@ def get_configs(settings, work_dir):
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/regnet_x_400mf_tv_{hr_input_sizes_x[0]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         'cl-6122':utils.dict_update(common_cfg,
             preprocess=preproc_transforms.get_transform_onnx(reverse_channels=True, resize=hr_input_sizes[1], crop=hr_input_sizes[1]),
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/regnet_x_400mf_tv_{hr_input_sizes_x[1]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         # pycls: classification regnetx800mf_224x224 expected_metric: 75.2% top-1 accuracy
         'cl-6131':utils.dict_update(common_cfg,
@@ -163,14 +163,14 @@ def get_configs(settings, work_dir):
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/regnet_x_800mf_tv_{hr_input_sizes_x[0]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         'cl-6132':utils.dict_update(common_cfg,
             preprocess=preproc_transforms.get_transform_onnx(reverse_channels=True, resize=hr_input_sizes[1], crop=hr_input_sizes[1]),
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/regnet_x_800mf_tv_{hr_input_sizes_x[1]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         # pycls: classification regnetx1.6gf_224x224 expected_metric: 77.0% top-1 accuracy
         'cl-6141':utils.dict_update(common_cfg,
@@ -178,14 +178,14 @@ def get_configs(settings, work_dir):
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/regnet_x_1_6gf_tv_{hr_input_sizes_x[0]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
         'cl-6142':utils.dict_update(common_cfg,
             preprocess=preproc_transforms.get_transform_onnx(reverse_channels=True, resize=hr_input_sizes[1], crop=hr_input_sizes[1]),
             session=onnx_session_type(**sessions.get_onnx_session_cfg(settings, work_dir=work_dir),
                 runtime_options=utils.dict_update(settings.runtime_options_onnx_np2(), hr_runtime_options),
                 model_path=f'{settings.models_path}/vision/high_resolution/imagenet1k/torchvision/regnet_x_1_6gf_tv_{hr_input_sizes_x[1]}.onnx'),
-            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=None)
+            model_info=dict(metric_reference={'accuracy_top1%':None}, model_shortlist=60)
         ),
     }
     return pipeline_configs
