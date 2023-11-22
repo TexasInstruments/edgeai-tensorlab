525677a5d9332e0c9d95371f4cf5e5e26ff2d0dd
Manu Mathew <a0393608@udtensorlab9>
2023-11-22 21:18:05 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -26,6 +26,78 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from edgeai_benchmark import setup
+import sys
+import os
+import importlib
+import subprocess
+from setuptools import setup, Extension, find_packages
 
-setup.main()
+
+def git_hash():
+    git_path = './' if os.path.exists('.git') else ('../' if os.path.exists('../.git') else None)
+    if git_path:
+        hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+        return hash[:7] if (hash is not None) else None
+    else:
+        return None
+
+
+def import_file_folder(file_or_folder_name):
+    if file_or_folder_name.endswith(os.sep):
+        file_or_folder_name = file_or_folder_name[:-1]
+    #
+    parent_folder = os.path.dirname(file_or_folder_name)
+    basename = os.path.splitext(os.path.basename(file_or_folder_name))[0]
+    sys.path.insert(0, parent_folder)
+    imported_module = importlib.import_module(basename, __name__)
+    sys.path.pop(0)
+    return imported_module
+
+
+def get_version():
+    version_file = os.path.realpath(os.path.join(os.path.dirname(__file__), 'version.py'))
+    print(f"version_file={version_file}")
+    version = import_file_folder(version_file)
+    hash = git_hash()
+    version_str = version.__version__ + '+' + hash.strip().decode('ascii') if (hash is not None) else version.__version__
+    return version_str
+
+
+def main():
+    version_str = get_version()
+
+    requirements_file = os.path.realpath(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
+    with open(requirements_file) as fp:
+        requirements = fp.read().splitlines()
+
+    readme_file = os.path.realpath(os.path.join(os.path.dirname(__file__), 'README.md'))
+    with open(readme_file,  encoding="utf8") as readme:
+        long_description = readme.read()
+
+    setup(
+        name='edgeai_benchmark',
+        version=version_str,
+        description='Edge AI Benchmark',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://bitbucket.itg.ti.com/projects/EDGEAI-ALGO/repos/edgeai-benchmark/browse',
+        author='EdgeAI, TIDL & Analytics Algo Teams',
+        author_email='edgeai-devkit@list.ti.com',
+        classifiers=[
+            'Development Status :: 4 - Beta'
+            'Programming Language :: Python :: 3.7'
+        ],
+        keywords = 'artifical intelligence, deep learning, image classification, object detection, semantic segmentation, quantization',
+        python_requires='>=3.7',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=requirements,
+        project_urls={
+            'Source': 'https://bitbucket.itg.ti.com/projects/EDGEAI-ALGO/repos/edgeai-benchmark/browse',
+            'Bug Reports': 'https://e2e.ti.com/support/processors/f/791/tags/jacinto_2D00_ai_2D00_devkit',
+        },
+    )
+
+
+if __name__ == '__main__':
+    main()
