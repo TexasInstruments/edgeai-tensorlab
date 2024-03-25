e614b39907fd27932ebf91611237675ab9d35e15
Manu Mathew <a0393608@udtensorlab9>
2024-03-25 16:33:26 +0530

    bash script warning is printed and script is paused for confirmation.

--- run_benchmarks_pc.sh
+++ run_benchmarks_pc.sh
@@ -29,6 +29,10 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ##################################################################
+echo "=> Recommend to use the alternate script run_benchmarks_parallelbash_pc.sh as back-to-back import followed by inference doesnt work with this script."
+read -p "=> OR press any key to continue..."
+
+##################################################################
 # target_device - use one of: TDA4VM AM62A AM68A AM69A
 # (Note: until r8.5 only TDA4VM was supported)
 TARGET_SOC=${1:-TDA4VM}
