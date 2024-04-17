4c81cd45bfad3b67e0dff106c11e212e0d85f21f
Manu Mathew <a0393608@udtensorlab9>
2024-04-17 20:38:08 +0530

    quant_fx_fun - using init function instead of a wrapper class

--- torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/__init__.py
+++ torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/__init__.py
@@ -34,5 +34,7 @@ from .observer_types import *
 from .fake_quanitze_types import *
 from .qconfig_types import *
 from .quant_fx_base import *
-from .quant_fx import *
+from .quant_fx_module import *
+
+from . import quant_fx_func
 
---
+++ torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/quant_fx_func.py
@@ -0,0 +1,191 @@
+#################################################################################
+# Copyright (c) 2018-2023, Texas Instruments Incorporated - http://www.ti.com
+# All Rights Reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#################################################################################
+
+import os
+import copy
+import torch
+from torch.ao.quantization import quantize_fx
+from torch.ao.quantization import QConfigMapping
+from torch.ao.quantization import FakeQuantize
+import statistics
+import functools
+import types
+
+from .... import xnn
+
+from . import observer_types
+from . import fake_quanitze_types
+from . import qconfig_types
+
+
+class ModelQuantFormat:
+    FLOAT_MODEL = "FLOAT_MODEL"
+    FAKEQ_MODEL = "FAKEQ_MODEL"
+    QDQ_MODEL = "QDQ_MODEL"
+    INT_MODEL = "INT_MODEL"
+    _NUM_FORMATS_ = 4
+
+
+def freeze(self, freeze_bn=True, freeze_observers=True):
+    if freeze_observers is True:
+        self.apply(torch.ao.quantization.disable_observer)
+    elif freeze_observers is False:
+        self.apply(torch.ao.quantization.enable_observer)
+    #
+    if freeze_bn is True:
+        self.apply(torch.nn.intrinsic.qat.freeze_bn_stats)
+    elif freeze_bn is False:
+        self.apply(torch.nn.intrinsic.qat.update_bn_stats)
+    #
+    return self
+
+def unfreeze(self, freeze_bn=False, freeze_observers=False):
+    freeze(self, freeze_bn, freeze_observers)
+    return self
+
+def forward(self, *input, **kwargs):
+    return self(*input, **kwargs)
+
+def convert(self, device='cpu', model_quant_format=None, convert_custom_config=None, backend_config=None):
+    freeze(self)
+    # convert requires cpu model
+    self.to(torch.device(device))
+    # now do the actual conversion
+    self = quantize_fx.convert_fx(self, convert_custom_config=convert_custom_config, backend_config=backend_config)
+    return self
+
+def export(self, example_input, filename='model.onnx', opset_version=17, model_quant_format=None, preserve_qdq_model=True,
+           simplify=False, skipped_optimizers=None):
+    if model_quant_format == ModelQuantFormat.INT_MODEL:
+        # # Convert QDQ format to Int8 format
+        import onnxruntime as ort
+        qdq_filename = os.path.splitext(filename)[0] + '_qdq.onnx'
+        torch.onnx.export(self, example_input, qdq_filename, opset_version=opset_version)
+        so = ort.SessionOptions()
+        so.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+        so.optimized_model_filepath = filename
+        # logger.info("Inplace conversion of QDQ model to INT8 model at: {}".format(onnx_file))
+        ort.InferenceSession(qdq_filename, so)
+        if not preserve_qdq_model:
+            os.remove(qdq_filename)
+        #
+    else:
+        torch.onnx.export(self, example_input, filename, opset_version=opset_version)
+    #
+    if simplify:
+        import onnx
+        from onnxsim import simplify
+        onnx_model = onnx.load(filename)
+        onnx_model, check = simplify(onnx_model, skipped_optimizers=skipped_optimizers)
+        onnx.save(onnx_model, filename)
+    #
+
+def train(self, mode: bool = True):
+    # put the model in expected mode
+    self.__train_backup__(mode=mode)
+    # also freeze the params if required
+    if mode is True:
+        # set the default epoch at which freeze occurs during training (if missing)
+        num_batch_norm_update_epochs = self.__state_params__.num_batch_norm_update_epochs or ((self.__state_params__.total_epochs//2)-1)
+        num_observer_update_epochs = self.__state_params__.num_observer_update_epochs or ((self.__state_params__.total_epochs//2)+1)
+        freeze_bn = (self.__state_params__.num_epochs_tracked >= num_batch_norm_update_epochs)
+        freeze_observers = (self.__state_params__.num_epochs_tracked >= num_observer_update_epochs)
+        if freeze_bn:
+            xnn.utils.print_once('Freezing BN for subsequent epochs')
+        #
+        if freeze_observers:
+            xnn.utils.print_once('Freezing ranges for subsequent epochs')
+        #
+        freeze(self, freeze_bn=freeze_bn, freeze_observers=freeze_observers)
+        # adjust_gradual_quantization_wrapper(self)
+        self.__state_params__.num_epochs_tracked += 1
+    else:
+        freeze(self)
+    #
+    return self
+
+
+def init(model, *args, qconfig_type=None, example_inputs=None, is_qat=True, backend="qnnpack",
+                 total_epochs=0, num_batch_norm_update_epochs=None, num_observer_update_epochs=None,
+                 qconfig_mode=qconfig_types.QConfigMode.DEFAULT, add_methods_to_model=True, **kwargs):
+
+    if not total_epochs:
+        raise RuntimeError("total_epochs must be provided")
+    #
+    # handle None here
+    qconfig_type = qconfig_type or qconfig_types.QConfigType.DEFAULT
+    # split based on + for mixed precision
+    qconfig_type = qconfig_type.split("+") if isinstance(qconfig_type, str) else (qconfig_type,)
+    if len(qconfig_type) > 2:
+        raise RuntimeError(f"maximum of 2 entries are supported in qconfig_type:{qconfig_type}")
+    #
+
+    qconfig_mapping = qconfig_types.get_qconfig_mapping(is_qat, backend, qconfig_type)
+    if is_qat:
+        model = quantize_fx.prepare_qat_fx(model, qconfig_mapping, example_inputs)
+    else:
+        model = quantize_fx.prepare_fx(model, qconfig_mapping, example_inputs)
+    #
+    model = qconfig_types.adjust_mixed_precision_qconfig(model, is_qat, backend, qconfig_type)
+
+    # a place to put all state variables
+    model.__state_params__ = xnn.utils.AttrDict()
+
+    # other parameters
+    model.__state_params__.is_qat = is_qat
+    model.__state_params__.backend = backend
+    model.__state_params__.qconfig_type = qconfig_type
+    model.__state_params__.num_batch_norm_update_epochs = num_batch_norm_update_epochs
+    model.__state_params__.num_observer_update_epochs = num_observer_update_epochs
+    model.__state_params__.num_epochs_tracked = 0
+    model.__state_params__.total_epochs = total_epochs
+
+    # set the quantization backend - qnnpack, fbgemm, x86, onednn etc.
+    if backend not in torch.backends.quantized.supported_engines:
+        raise RuntimeError("Quantized backend not supported: " + str(backend))
+    torch.backends.quantized.engine = backend
+
+    # related to adaptive quantization
+    model.__state_params__.qconfig_mode = qconfig_types.QConfigMode(qconfig_mode)
+    model.__state_params__.forzen_layer_names_list = []
+
+    if add_methods_to_model:
+        # add a wrapper for model.train()
+        model.__train_backup__ = types.MethodType(model.train.__func__, model)
+        model.train = types.MethodType(train, model)
+        # other methods
+        model.freeze = types.MethodType(freeze, model)
+        model.unfreeze = types.MethodType(unfreeze, model)
+        model.convert = types.MethodType(convert, model)
+        model.export = types.MethodType(export, model)
+    #
+    return model
+
