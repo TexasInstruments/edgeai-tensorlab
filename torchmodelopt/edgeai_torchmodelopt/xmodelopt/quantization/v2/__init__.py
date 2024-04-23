c3e0185aabffd83bdb4e177d4e88608ca71af240
Parakh Agarwal <p-agarwal@ti.com>
2024-04-23 15:39:37 +0530

    added changed attention layer

--- torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/__init__.py
+++ torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/__init__.py
@@ -35,5 +35,6 @@ from .fake_quanitze_types import *
 from .qconfig_types import *
 from .quant_fx_base import *
 from .quant_fx_module import *
+from .quant_fx_utils import *
 
 from . import quant_fx_func
