17a33a738a39ca99c0953ada0e0c118e4a4bab7e
Parakh Agarwal <p-agarwal@ti.com>
2024-04-23 15:39:37 +0530

    added changed attention layer

--- torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/fx/quant_fx_utils.py
+++ torchmodelopt/edgeai_torchmodelopt/xmodelopt/quantization/v2/fx/quant_fx_utils.py
@@ -39,10 +39,7 @@ import statistics
 import functools
 from torch.onnx import symbolic_helper
 from torch.onnx._internal import jit_utils
-<<<<<<< HEAD
 from torch import nn
-=======
->>>>>>> dd585c7 (fix quantized export)
 
 from ..... import xnn
 
@@ -247,4 +244,46 @@ class QuantLayerNorm(torch.nn.Module):
         return y
 =======
     return symbolic_helper.quantize_helper(g, output, op_scale, op_zero_point)
->>>>>>> dd585c7 (fix quantized export)
+
+# similar class as to default attention, however it supports model export as well as quantizing matmul operation
+class QuantAttention(nn.Module):
+    
+    def __init__(
+            self,
+            dim: int = 128,
+            num_heads: int = 8,
+            qkv_bias: bool = False,
+            qk_norm: bool = False,
+            attn_drop: float = 0.,
+            proj_drop: float = 0.,
+            norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, N, C = x.shape
+        qkv = torch.permute(self.qkv(x).reshape(B, N, 3, -1, self.head_dim), (2, 0, 3, 1, 4))
+        q, k, v = qkv.unbind(0)
+        q, k = self.q_norm(q), self.k_norm(k)
+
+        q = torch.mul(q, torch.tensor(self.scale))
+        attn = torch.matmul(q, k.transpose(-2, -1))
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        x = torch.matmul(attn, v)
+
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
