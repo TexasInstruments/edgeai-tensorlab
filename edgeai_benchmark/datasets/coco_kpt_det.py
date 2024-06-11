c0b937615f5245a30b3ae9db62ea5f4aee23cd8f
Manu Mathew <a0393608@udtensorlab9>
2024-06-11 13:11:44 +0530

    dataset_info - now returns instance instead of copy

--- edgeai_benchmark/datasets/coco_kpt_det.py
+++ edgeai_benchmark/datasets/coco_kpt_det.py
@@ -253,6 +253,9 @@ class COCOKeypointDetection(DatasetBase):
         return accuracy
 
     def get_dataset_info(self):
+        if 'dataset_info' in self.kwargs:
+            return self.kwargs['dataset_info']
+        #
         # return only info and categories for now as the whole thing could be quite large.
         dataset_store = dict()
         for key in ('info', 'categories'):
