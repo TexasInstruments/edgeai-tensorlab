7ca4fae28fce620238dab77dac88506ee00ff114
Rekib Zaman <r-zaman@ti.com>
2024-06-26 16:31:39 +0530

    updated retinanet_regnet config

--- configs_edgeailite/retinanet/retinanet_regnet_fpn_bgr_lite.py
+++ configs_edgeailite/retinanet/retinanet_regnet_fpn_bgr_lite.py
@@ -1,30 +1,5 @@
-######################################################
-input_size = (512,512)                          #(320,320) #(384,384) #(512,512) #(768,384) #(768,768) #(1024,512) #(1024,1024)
-dataset_type = 'CocoDataset'
-num_classes_dict = {'CocoDataset':80, 'VOCDataset':20, 'CityscapesDataset':8, 'WIDERFaceDataset':1}
-num_classes = num_classes_dict[dataset_type]
-img_norm_cfg = dict(mean=[103.53, 116.28, 123.675], std=[57.375, 57.12, 58.395], to_rgb=False) #imagenet mean used in pycls (bgr)
-
-_base_ = [
-    f'../_xbase_/datasets/{dataset_type.lower()}.py',
-    '../_xbase_/hyper_params/common_config.py',
-    '../_xbase_/hyper_params/retinanet_config.py',
-    '../_xbase_/hyper_params/common_schedule.py',
-]
-
-######################################################
-# settings for qat or calibration - uncomment after doing floating point training
-# also change dataset_repeats in the dataset config to 1 for fast learning
-quantize = False #'training' #'calibration'
-initial_learning_rate = 8e-2
-samples_per_gpu = 16
-if quantize:
-  load_from = './work_dirs/retinanet_regnet_fpn_bgr_lite/latest.pth'
-  optimizer = dict(type='SGD', lr=initial_learning_rate/100.0, momentum=0.9, weight_decay=4e-5) #1e-4 => 4e-5
-  total_epochs = 1 if quantize == 'calibration' else 12
-else:
-  optimizer = dict(type='SGD', lr=initial_learning_rate, momentum=0.9, weight_decay=4e-5) #1e-4 => 4e-5
-#
+_base_ = ['../../configs/_base_/schedules/schedule_1x.py', '../../configs/_base_/default_runtime.py']
+input_size = (512,512) 
 
 ######################################################
 backbone_type = 'RegNet'
@@ -88,13 +63,22 @@ input_size_divisor = 32
 
 conv_cfg = None
 norm_cfg = dict(type='BN')
-convert_to_lite_model = dict(group_size_dw=regnet_cfg['group_size_dw'])
+convert_to_lite_model = dict(group_size_dw=regnet_cfg['group_size_dw'], model_surgery=1)
 
 #head_base_stride = (8 if fpn_start_level==1 else (4 if fpn_start_level==0 else None))
 head_stacked_convs = regnet_cfg['head_stacked_convs']
 
+
+# model settings
+data_preprocessor = dict(
+    type='DetDataPreprocessor',
+    mean=[103.53, 116.28, 123.675],
+    std=[57.375, 57.12, 58.395],
+    bgr_to_rgb=False,
+    pad_size_divisor=32)
 model = dict(
     type='RetinaNet',
+    data_preprocessor=data_preprocessor,
     backbone=dict(
         type=backbone_type,
         arch=backbone_arch,
@@ -114,7 +98,7 @@ model = dict(
         norm_cfg=norm_cfg),
     bbox_head=dict(
         type='RetinaHead',
-        num_classes=num_classes,
+        num_classes=80,
         in_channels=fpn_out_channels,
         stacked_convs=head_stacked_convs,
         feat_channels=fpn_out_channels,
@@ -136,55 +120,122 @@ model = dict(
             gamma=2.0,
             alpha=0.25,
             loss_weight=1.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)))
-
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='GridAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.5,
+            min_pos_iou=0)),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        conf_thr=0.005,
+        nms=dict(type='nms', iou_threshold=0.45),
+        max_per_img=100))
 # dataset settings
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+
+# Example to use different file client
+# Method 1: simply set the data root and let the file I/O module
+# automatically infer from prefix (not support LMDB and Memcache yet)
+
+# data_root = 's3://openmmlab/datasets/detection/coco/'
+
+# Method 2: Use `backend_args`, `file_client_args` in versions before 3.0.0rc6
+# backend_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/detection/',
+#         'data/': 's3://openmmlab/datasets/detection/'
+#     }))
+backend_args = None
+
 train_pipeline = [
-    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadImageFromFile', backend_args=backend_args),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='PhotoMetricDistortion',
-        brightness_delta=32,
-        contrast_range=(0.5, 1.5),
-        saturation_range=(0.5, 1.5),
-        hue_delta=18) if not quantize else dict(type='Bypass'),
-    dict(
-        type='Expand',
-        mean=img_norm_cfg['mean'],
-        to_rgb=img_norm_cfg['to_rgb'],
-        ratio_range=(1, 4)),
+    # `mean` and `to_rgb` should be the same with the `preprocess_cfg`
+    dict(type='Expand', mean=[0, 0, 0], to_rgb=True, ratio_range=(1, 4)),
     dict(
         type='MinIoURandomCrop',
         min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
         min_crop_size=0.3),
-    dict(type='Resize', img_scale=input_size, keep_ratio=False),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=input_size_divisor),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='RandomResize', scale=input_size, keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(type='PackDetInputs')
 ]
-
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=input_size, keep_ratio=False),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(
-        type='MultiScaleFlipAug',
-        img_scale=input_size,
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=False),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=input_size_divisor),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
 ]
 
-data = dict(
-    samples_per_gpu=samples_per_gpu,
-    workers_per_gpu=0,
-    train=dict(dataset=dict(pipeline=train_pipeline)),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='val2017/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+test_dataloader = val_dataloader
 
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/instances_val2017.json',
+    metric='bbox',
+    backend_args=backend_args)
+test_evaluator = val_evaluator
+
+train_cfg = dict(max_epochs=273, val_interval=7)
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005),
+    clip_grad=dict(max_norm=35, norm_type=2))
+
+# learning policy
+param_scheduler = [
+    dict(type='LinearLR', start_factor=0.1, by_epoch=False, begin=0, end=2000),
+    dict(type='MultiStepLR', by_epoch=True, milestones=[218, 246], gamma=0.1)
+]
+
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=7))
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (8 samples per GPU)
+auto_scale_lr = dict(base_batch_size=64)
