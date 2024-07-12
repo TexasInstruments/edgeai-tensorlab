e0f00a740529006d71b4cc403a6484c467634822
Rekib Zaman <r-zaman@ti.com>
2024-07-12 17:14:56 +0530

    fix in pytorch2onnx

--- mmdeploy/apis/pytorch2onnx.py
+++ mmdeploy/apis/pytorch2onnx.py
@@ -24,7 +24,6 @@ def torch2onnx(img: Any,
                model_cfg: Union[str, mmengine.Config],
                model_checkpoint: Optional[str] = None,
                device: str = 'cuda:0',
-               model_surgery: Any = None,
                torch_model = None):
     """Convert PyTorch model to ONNX model.
 
@@ -71,8 +70,8 @@ def torch2onnx(img: Any,
     from mmdeploy.apis import build_task_processor
     task_processor = build_task_processor(model_cfg, deploy_cfg, device)
 
-    # torch_model = task_processor.build_pytorch_model(model_checkpoint)
-    # torch_model = build_model_from_cfg(model_cfg, model_checkpoint, device)
+    if torch_model is None:
+        torch_model = task_processor.build_pytorch_model(model_checkpoint)
 
     input_metas=None
     if not isinstance(img, str):
@@ -107,15 +106,6 @@ def torch2onnx(img: Any,
     keep_initializers_as_inputs = onnx_cfg.get('keep_initializers_as_inputs',
                                                True)
     optimize = onnx_cfg.get('optimize', False)
-
-    # model surgery should be implemented here if not implemented in mmdetection or mmpose
-    # if model_surgery is None:
-    #     if hasattr(model_cfg,'convert_to_lite_model'):
-    #         model_surgery = model_cfg.convert_to_lite_model.model_surgery
-    # model surgery
-    # if model_surgery == 1:
-    #     torch_model = convert_to_lite_model(torch_model, model_cfg)
-    # load_checkpoint(torch_model, model_checkpoint, map_location='cpu')
 
     if backend == Backend.NCNN.value:
         """NCNN backend needs a precise blob counts, while using onnx optimizer
