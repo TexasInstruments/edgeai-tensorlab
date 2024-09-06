b413196816fe3b7cc31c7215ffba3b36c74bd92a
Kunal Patel <k-patel1@ti.com>
2024-09-06 13:37:27 +0530

    v3 get_source_partition modified to separate shared module partitions and use same module for replacement

--- torchmodelopt/edgeai_torchmodelopt/xmodelopt/surgery/v3/replacer.py
+++ torchmodelopt/edgeai_torchmodelopt/xmodelopt/surgery/v3/replacer.py
@@ -60,7 +60,7 @@ def _remove_hanging_nodes(main_module:GraphModule):
     def find_hanging_nodes(main_module:GraphModule):
         count =[]
         for node in main_module.graph.nodes:
-            if (node.op != 'output' and len(node.users)==0):
+            if (node.op not in ('output','placeholder') and len(node.users)==0):
                 count.append(node)
         return count
     h_nodes=find_hanging_nodes(main_module)
@@ -96,8 +96,19 @@ def adjust_meta(new_node:fx.Node, old_node:fx.Node, replace_node:fx.Node,partiti
             new_node.meta[meta_name] = dict(old_meta_val[:index]+replace_meta_val)
 
 
+def _get_partition_string(main_module:fx.GraphModule, partition:SourcePartition):
+    modules = dict(main_module.named_modules())
+    string = ''
+    for i,node in enumerate(partition.nodes):
+        string += f'{node.target}'
+        if node.op == 'call_module':
+            string += f'_{modules[node.target]}'
+        if i < (len(partition.nodes) -1):
+            string += '_'
+    return string 
+
 # replaces a pattern fom start to end in graph module to a call_module node
-def _replace_pattern(main_module:GraphModule, partition:SourcePartition, replace_module:ReplacedModule, no_of_module_replaced:int=0, node_mapping:dict[fx.Node,fx.Node] = None ):
+def _replace_pattern(main_module:GraphModule, partition:SourcePartition, replace_module:ReplacedModule, no_of_module_replaced:int=0, partition_module_map:dict[str,dict[str,tuple[str,nn.Module|nn.Parameter|torch.Tensor|Any]]] = {}):
     '''
     replaces nodes from a partition with the nodes of replacement module if it is graph module
     if it's a module other than a graph module it will add that to the main module and add it as call module to the graph
@@ -112,6 +123,10 @@ def _replace_pattern(main_module:GraphModule, partition:SourcePartition, replace
     total_nodes = list(main_module.graph.nodes)
     main_output_nodes.sort(key=lambda node:total_nodes.index(node))
     replace_modules = dict(replace_module.module.named_modules())
+    
+    partition_string = _get_partition_string(main_module, partition)
+    partition_module = partition_module_map.setdefault(partition_string,{})
+    
     if isinstance(replace_module.module,GraphModule):
         num_param_added = num_attr_added = num_module_added = 0
         node_mapping = node_mapping or {}
@@ -139,22 +154,38 @@ def _replace_pattern(main_module:GraphModule, partition:SourcePartition, replace
                     new_node = main_module.graph.call_function(node.target,args,node.kwargs)
                 
                 elif node.op == 'get_attr':
-                    attr = getattr(replace_module.module, node.target)
-                    if isinstance(attr, nn.Parameter):
-                        attr_name = f'_param_constant{num_param_added}_{no_of_module_replaced}'
-                        main_module.register_parameter(attr_name,copy.deepcopy(attr))
-                        num_param_added += 1
+                    orig_attr = getattr(replace_module.module, node.target)
+                    if isinstance(orig_attr, nn.Parameter):
+                        key = f'_param_constant{num_param_added}'
+                        is_key_new = key not in partition_module
+                        attr_name = f'{key}_{no_of_module_replaced}'
+                        attr_name, attr = partition_module.setdefault(key, (attr_name,orig_attr))
+                        if is_key_new:
+                            main_module.register_parameter(attr_name,copy.deepcopy(attr))
+                            num_param_added += 1
                     else:
-                        attr_name = f'_attr_constant{num_attr_added}_{no_of_module_replaced}'
-                        main_module.register_buffer(attr_name,attr)
-                        num_attr_added += 1
+                        key = f'_attr_constant{num_attr_added}'
+                        attr_name = f'{key}_{no_of_module_replaced}'
+                        is_key_new = key not in partition_module
+                        attr_name, attr = partition_module.setdefault(key, (attr_name,orig_attr))
+                        if is_key_new:
+                            if isinstance(attr, torch.Tensor):
+                                main_module.register_buffer(attr_name,attr)
+                            else:
+                                setattr(main_module,attr_name,attr)
+                            num_attr_added += 1
                     new_node = main_module.graph.get_attr(attr_name)
                 
                 elif node.op == 'call_module':
-                    module = copy.deepcopy(replace_modules[node.target])
-                    module_name = f'replaced_{partition.source.__name__}_{node.target}_{num_module_added}_{no_of_module_replaced}'
-                    num_module_added += 1
-                    main_module.add_module(module_name,module)
+                    orig_module = copy.deepcopy(replace_modules[node.target])
+                    source = partition.source
+                    key = f'replaced_{source if isinstance(source,str) else source.__name__}_{num_module_added}'
+                    is_key_new = key not in partition_module
+                    module_name = f'{key}_{no_of_module_replaced}'
+                    module_name, module = partition_module.setdefault(key,(module_name,orig_module))
+                    if is_key_new:
+                        main_module.add_module(module_name,module)
+                        num_module_added += 1
                     new_node = main_module.graph.call_module(module_name,args,node.kwargs)
                 
                 node_mapping[node] = new_node
@@ -191,7 +222,10 @@ def _replace_all_matches(main_module:GraphModule, pattern_partitions:list[Source
         return inputs,{}
     
     global __net_module_replaced
+    partition_module_map = {}
     for partition in pattern_partitions:
+        partition_string = _get_partition_string(main_module, partition)
+        is_new = partition_string not in partition_module_map
         source = partition.source
         for i,replacement in enumerate(replacements):
             replace_module, input_adjustment_func =replacement
@@ -202,10 +236,13 @@ def _replace_all_matches(main_module:GraphModule, pattern_partitions:list[Source
                                                 )
             if replace_module_copy.module is not None:
                 break
-        
+        temp_partition_module_map = {}
+        temp_net_module_replaced = 0
         for replacement in replacements[(i+1):]:
             replace_module, input_adjustment_func =replacement
             temp_partitions = utils.get_source_partition(replace_module_copy.module.graph,[source])[source]
+            temp_partition_string = _get_partition_string(replace_module_copy.module, temp_partition)
+            is_temp_new = temp_partition_string not in temp_partition_module_map
             for temp_partition in temp_partitions:
                 temp_replace_module_copy = ReplacedModule(replace_module_copy.module, temp_partition, 
                                                     gen_func=default_module_gen_func if isinstance(replace_module,nn.Module) else replace_module,
@@ -214,15 +251,19 @@ def _replace_all_matches(main_module:GraphModule, pattern_partitions:list[Source
                                                     )
                 if temp_replace_module_copy.module is None:
                     continue
-                _replace_pattern(replace_module_copy.module,temp_partition,temp_replace_module_copy)
+                _replace_pattern(replace_module_copy.module,temp_partition,temp_replace_module_copy,temp_net_module_replaced,temp_partition_module_map)
+                if is_temp_new:
+                    temp_net_module_replaced += 1
         if replace_module_copy.module is None:
             continue 
         _remove_hanging_nodes(replace_module_copy.module)
         # replace_module_copy = replace_module_copy.to(main_module)
             
-        _replace_pattern(main_module, partition, replace_module_copy, __net_module_replaced)
-        __net_module_replaced = __net_module_replaced + 1
+        _replace_pattern(main_module, partition, replace_module_copy, __net_module_replaced,partition_module_map)
+        if is_new:
+            __net_module_replaced = __net_module_replaced + 1
     
+    _remove_hanging_nodes(main_module)
     main_module.graph.lint()
     main_module.recompile()
 
