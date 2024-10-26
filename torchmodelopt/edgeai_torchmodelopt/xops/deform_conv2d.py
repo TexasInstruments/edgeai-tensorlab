c301730e5e7becc308af8aafa3469dedc5ebb9c3
Manu <a0393608@udtensorlab9>
2024-10-26 23:25:46 +0530

    DCNv2 module added - test of this is failing as of now.

--- torchmodelopt/edgeai_torchmodelopt/xops/deform_conv2d.py
+++ torchmodelopt/edgeai_torchmodelopt/xops/deform_conv2d.py
@@ -40,7 +40,10 @@ class DeformConvOp2d(torchvision.ops.DeformConv2d):
         '''
         Deformable convolution operator (derived from torchvision.ops.DeformConv2d)
         '''
+        mode = kwargs.pop('mode', 'bilinear')
+        assert mode in ('bilinear',), 'mode should be one of: bilinear, nearest'
         super().__init__(*args, **kwargs)
+        self.mode = mode
         warnings.warn('DeformConvOp2d or torchvision.ops.DeformConv2d is not our recommended '
             'Deformable Convolution implementation. Please use DeformConvWithGS2d instead.')
 
@@ -49,10 +52,10 @@ class DeformConvWithGS2d(torchvision.ops.DeformConv2d):
     '''
     Deformable convolution with Grid Sample
     '''
-    def __init__(self, *args, mode='bilinear', **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        mode = kwargs.pop('mode', 'bilinear')
         assert mode in ('bilinear', 'nearest'), 'mode should be one of: bilinear, nearest'
-
+        super().__init__(*args, **kwargs)
         self.mode = mode
 
     def forward(self, feat, offset, mask):
@@ -138,8 +141,52 @@ class DeformConvWithGS2d(torchvision.ops.DeformConv2d):
         return out
 
 
+class DCNv2(torch.nn.Module):
+    """
+    DCNv2 - Deformable Convolution Layer
 
-if __name__ == "__main__":
+    Includes a regular convolution that generates the offset, mask
+    and also the deformable convolution that uses it
+    """
+    def __init__(self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1,
+        groups: int = 1,
+        bias: bool = True,
+        mode: str = 'bilinear',
+        deform_groups: int = 1,
+        deform_layer_type = None):
+
+        super().__init__()
+
+        deform_layer_type = deform_layer_type or DeformConvWithGS2d
+
+        ks = (kernel_size,kernel_size) if isinstance(kernel_size, int) else kernel_size
+        self.mask_out_channels = deform_groups * ks[0] * ks[1]
+        self.offset_out_channels = self.mask_out_channels*2
+
+        self.conv_offset = torch.nn.Conv2d(in_channels, (self.offset_out_channels+self.mask_out_channels),
+                kernel_size, stride, padding, dilation, groups, bias)
+
+        self.conv_deform = deform_layer_type(in_channels, out_channels,
+                kernel_size, stride, padding, dilation, groups, bias, mode=mode)
+
+    def forward(self, feat):
+        offset_mask = self.conv_offset(feat)
+        offset_yx = offset_mask[:,:self.offset_out_channels,...]
+        mask = offset_mask[:,self.offset_out_channels:,...]
+        return self.conv_deform(feat, offset_yx, mask)
+
+
+###########################################################################################
+# unit tests
+###########################################################################################
+
+def test_deform_op():
     # Set test params
     IN_HEIGHT = 58
     IN_WIDTH = 100
@@ -152,7 +199,7 @@ if __name__ == "__main__":
     STRIDE = 1
     PADDING = 1
     DILATION = 1
-    BIAS = False
+    BIAS = True
     DEFORM_GROUPS = 1
     INTP_MODE = 'bilinear'
 
@@ -203,12 +250,12 @@ if __name__ == "__main__":
     # Check differences between deform_op and deform_with_gs
     diff = out_deform_with_gs - out_deform_op
     if torch.sum((abs(diff) > 1e-4) == True) > 0:
-        print('deform_op and deform_with_gs do not match!\n')
+        test_output = False
     else:
-        print('deform_op and deform_with_gs matches\n')
+        test_output = True
 
     # Export deform_with_gs model
-    input_names  = ["feat", "offset_y", "offset_x", "mask"]
+    input_names  = ["feat", "offset", "mask"]
     output_names = ["output"]
     modelInput = (
         feat,
@@ -226,3 +273,108 @@ if __name__ == "__main__":
     import onnx
     onnx_model, simplify_ok = simplify("deform_conv_pytorch.onnx")
     onnx.save(onnx_model, "deform_conv_pytorch.onnx")
+
+    if test_output:
+        print('\n\ndeform_op and deform_with_gs matches')
+        print("DeformConv Op: Test PASSED")
+    else:
+        print('\n\ndeform_op and deform_with_gs do not match!')
+        assert test_output, "DeformConv Op: Test FAILED"
+
+
+def test_dcnv2():
+    # Set test params
+    IN_HEIGHT = 58
+    IN_WIDTH = 100
+    OUT_HEIGHT = IN_HEIGHT
+    OUT_WIDTH = IN_WIDTH
+
+    IN_CHANNEL = 256
+    OUT_CHANNEL = 256
+    KERNEL_SIZE = 3
+    STRIDE = 1
+    PADDING = 1
+    DILATION = 1
+    BIAS = True
+    GROUPS = 1
+    DEFORM_GROUPS = 1
+    INTP_MODE = 'bilinear'
+
+    deform_op = DCNv2(IN_CHANNEL,
+                     OUT_CHANNEL,
+                     kernel_size=KERNEL_SIZE,
+                     stride=STRIDE,
+                     padding=PADDING,
+                     dilation=DILATION,
+                     groups=GROUPS,
+                     bias=BIAS,
+                     deform_groups=DEFORM_GROUPS,
+                     deform_layer_type=DeformConvOp2d)
+
+    deform_with_gs  = DCNv2(IN_CHANNEL,
+                     OUT_CHANNEL,
+                     kernel_size=KERNEL_SIZE,
+                     stride=STRIDE,
+                     padding=PADDING,
+                     dilation=DILATION,
+                     groups=GROUPS,
+                     bias=BIAS,
+                     mode=INTP_MODE,
+                     deform_groups=DEFORM_GROUPS,
+                     deform_layer_type=DeformConvWithGS2d)
+
+    # For evaluation, make weigth and bias of two models the same
+    deform_with_gs.conv_offset.weight = deform_op.conv_offset.weight
+    deform_with_gs.conv_deform.weight = deform_op.conv_deform.weight
+    if deform_with_gs.conv_offset.bias is not None:
+        deform_with_gs.conv_offset.bias   = deform_op.conv_offset.bias
+    if deform_with_gs.conv_deform.bias is not None:
+        deform_with_gs.conv_deform.bias   = deform_op.conv_deform.bias
+
+    # Input to the model: feature map, offset_y, offste_x, mask
+    feat = torch.randn(1, IN_CHANNEL, IN_HEIGHT, IN_WIDTH) * 10
+
+    deform_op.eval()
+    with torch.no_grad():
+        out_deform_op = deform_op(feat)
+
+    # Run deform_with_gs
+    deform_with_gs.eval()
+    with torch.no_grad():
+        out_deform_with_gs = deform_with_gs(feat)
+
+    # Check differences between deform_op and deform_with_gs
+    diff = out_deform_with_gs - out_deform_op
+    if torch.sum((abs(diff) > 1e-4) == True) > 0:
+        test_output = False
+    else:
+        test_output = True
+
+    # Export deform_with_gs model
+    input_names  = ["feat"]
+    output_names = ["output"]
+    model_input = (feat,)
+    torch.onnx.export(deform_with_gs,
+                      model_input,
+                      "dcnv2_pytorch.onnx",
+                      input_names=input_names,
+                      output_names=output_names,
+                      opset_version=16)
+
+    # simplify the onnx model
+    from onnxsim import simplify
+    import onnx
+    onnx_model, simplify_ok = simplify("dcnv2_pytorch.onnx")
+    onnx.save(onnx_model, "dcnv2_pytorch.onnx")
+
+    if test_output:
+        print('\n\nDCNv2 with deform_op and and DCNv2 deform_with_gs matches')
+        print("DCNv2: Test PASSED")
+    else:
+        print('\n\nDCNv2 with deform_op and and DCNv2 deform_with_gs do not match!')
+        assert test_output, "DCNv2: Test FAILED"
+
+
+if __name__ == "__main__":
+    test_deform_op()
+    test_dcnv2()
