f5bffd91da121838b69e4131ee30bfc22a68973d
Abhay Chirania <a-chirania@ti.com>
2025-01-17 17:51:01 +0530

    TEST: Add UART based automatic testing for benchmark on EVM
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

---
+++ tests/evm_test/evm_scripts/model_infer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+################################################################################
+TARGET_DEVICE=${1:-"AM68A"}
+TIMEOUT=${2:-"120"}
+GENERATE_REPORT=${3:-"0"}
+MODEL_SELECTION=${4:-"null"}
+NUM_FRAMES=${5:-"0"}
+
+EXTRA_ARGS="--experimental_models True --additional_models True"
+if [ "$MODEL_SELECTION" != "null" ]; then
+    EXTRA_ARGS="${EXTRA_ARGS} --model_selection ${MODEL_SELECTION}"
+fi
+if [ "$NUM_FRAMES" != "0" ]; then
+    EXTRA_ARGS="${EXTRA_ARGS} --num_frames ${NUM_FRAMES}"
+fi
+
+echo "TARGET_DEVICE:${TARGET_DEVICE}"
+echo "TIMEOUT:${TIMEOUT}"
+echo "EXTRA_ARGS:${EXTRA_ARGS}"
+
+# target Inference Steps
+cd ~/edgeai-benchmark
+timeout -k 5 ${TIMEOUT} python3 ./scripts/benchmark_modelzoo.py settings_infer_on_evm.yaml --target_device ${TARGET_DEVICE} ${EXTRA_ARGS}
+if [ $? -eq 124 ]; then
+    echo "TIMEDOUT after ${TIMEOUT}s"
+    exit 1
+fi
+if [ "$GENERATE_REPORT" == "1" ]; then
+    echo "Generating test report..."
+    python3 ./scripts/generate_report.py settings_infer_on_evm.yaml
+fi
+
+echo "END_OF_MODEL_INFERENCE"
---
+++ tests/evm_test/src/relay_control.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+AnelRelayControl is responsible for switching ANEL Elektronik AG power switch.
+This requires an executable provided by anel, ip address of the switch and the port number
+"""
+
+import os
+import sys
+import time
+
+class AnelRelayControl():
+    def __init__(self, executable, ip_address, relay_number):
+        self.relay_exe = executable
+        self.relay_ip = ip_address
+        self.relay_number = relay_number
+        self.verify_relay()
+
+    def verify_relay(self):
+        print(f"[ Info ] Verifying relay configurations")
+        if self.relay_number < 1 and self.relay_number > 8:
+            print(f"[ Error ] Relay number invalid")
+            sys.exit(-1)
+        if not os.path.exists(self.relay_exe):
+            print(f"[ Error ] {self.relay_exe} does not exist. Please provide a valid executable")
+            sys.exit(-1)
+
+    def switch_relay(self, operation="toggle"):
+        print(f"[ Info ] Switching realy [operatopn={operation}]")
+        if operation == "off":
+            os.system(f"{self.relay_exe} {self.relay_ip},75,77,rel,{self.relay_number},off,admin,anel")
+        elif operation == "on":
+            os.system(f"{self.relay_exe} {self.relay_ip},75,77,rel,{self.relay_number},on,admin,anel")
+        elif operation == "toggle":
+            os.system(f"{self.relay_exe} {self.relay_ip},75,77,rel,{self.relay_number},off,admin,anel")
+            time.sleep(5)
+            os.system(f"{self.relay_exe} {self.relay_ip},75,77,rel,{self.relay_number},on,admin,anel")
+        else:
+            print(f"[ Error ] Invalid operation value for relay. Allowed values are (off,on,toggle)")
