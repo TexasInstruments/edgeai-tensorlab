8a83ee1ab5519d4dd58f6078df059132253b86df
Parakh Agarwal <p-agarwal@ti.com>
2025-02-06 18:43:14 +0530

    update tool path

--- setup_pc.sh
+++ setup_pc.sh
@@ -112,7 +112,7 @@ if [[ $TIDL_TOOLS_RELEASE_NAME == "latest" || $TIDL_TOOLS_RELEASE_NAME == "r10.1
   pip3 install --no-input https://software-dl.ti.com/jacinto7/esd/tidl-tools/10_01_00_02/OSRT_TOOLS/X86_64_LINUX/UBUNTU_22_04/onnxruntime_tidl-1.15.0-cp310-cp310-linux_x86_64.whl
   pip3 install --no-input https://software-dl.ti.com/jacinto7/esd/tidl-tools/10_00_08_00/OSRT_TOOLS/X86_64_LINUX/UBUNTU_22_04/tflite_runtime-2.12.0-cp310-cp310-linux_x86_64.whl
   # these are internal links for now
-  TIDL_TOOLS_DOWNLOAD_LINKS=("http://10.24.68.92/OSRT_TOOLS/am68pa/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/am68a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/am69a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/am62a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/am67a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz")
+  TIDL_TOOLS_DOWNLOAD_LINKS=("http://10.24.68.92/OSRT_TOOLS/10_01_04_00/am68pa/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/10_01_04_00/am68a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/10_01_04_00/am69a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/10_01_04_00/am62a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz" "http://10.24.68.92/OSRT_TOOLS/10_01_04_00/am67a/tidl_tools${TIDL_TOOLS_TYPE_SUFFIX}.tar.gz")
   for (( soc_idx=0; soc_idx<"${#TARGET_SOCS[@]}"; soc_idx++ )); do
     TARGET_SOC=${TARGET_SOCS[$soc_idx]}
     TIDL_TOOLS_DOWNLOAD_LINK=${TIDL_TOOLS_DOWNLOAD_LINKS[$soc_idx]}
