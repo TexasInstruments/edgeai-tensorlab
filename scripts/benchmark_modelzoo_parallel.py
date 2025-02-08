3d3ffd00007796240994a1261837264e5dbc9fb4
Manu <a0393608@udtensorlab9>
2025-02-08 16:52:34 +0530

    cosmetic prints updated.

--- scripts/benchmark_modelzoo_parallel.py
+++ scripts/benchmark_modelzoo_parallel.py
@@ -155,8 +155,7 @@ if __name__ == '__main__':
     ####################################################################
     try:
         if settings.target_machine == 'pc' and settings.parallel_devices is None:
-            print(f"INFO: in PC host emulation mode, tidl_tools can speedup model compilation using cuda gpus")
-            print(f"INFO: - cuda gpus will be used only if tidl_tools was installed using setup_cpu_gpu.sh")
+            print(f"INFO: in PC host emulation, cuda gpus can be used by tidl_tools_gpu (use setup_cpu_gpu.sh)")
             nvidia_smi_command = 'nvidia-smi --list-gpus | wc -l'
             proc = subprocess.Popen([nvidia_smi_command], stdout=subprocess.PIPE, shell=True)
             out_ret, err_ret = proc.communicate()
