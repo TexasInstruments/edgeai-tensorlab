73beca6d376ad9fac340db389f42fae98f77eb3c
Manu <a0393608@udtensorlab9>
2025-02-26 20:24:39 +0530

    download_datasets script added

---
+++ scripts/download_datasets.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2018-2021, Texas Instruments
+# All Rights Reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import argparse
+import warnings
+
+
+from edgeai_benchmark import *
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('settings_file', type=str, default=None)
+    parser.add_argument('--dataset_list', type=str, nargs='*')
+    return parser
+
+
+if __name__ == '__main__':
+    print(f'argv: {sys.argv}')
+    # the cwd must be the root of the respository
+    if os.path.split(os.getcwd())[-1] == 'scripts':
+        os.chdir('../')
+    #
+
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    assert args.dataset_list is not None, f'dataset_list must be provided: {args.dataset_list}'
+
+    kwargs = vars(args)
+    settings_file = kwargs.pop('settings_file')
+    dataset_list = kwargs.pop('dataset_list')
+
+    settings = config_settings.ConfigSettings(settings_file, **kwargs)
+
+    if 'imagenetv2c' in dataset_list:
+        settings.dataset_type_dict ={'imagenet': 'imagenetv2c'}
+        replacements = {key:entry['category'] for key, entry in datasets.dataset_info_dict.items()}
+        dataset_list = [replacements.get(entry, entry) for entry in dataset_list]
+    #
+
+    datasets.download_datasets(settings, dataset_list=dataset_list)
