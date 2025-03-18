131b6f7cc781c86e0a9266d2fa728db0f0761453
Manu <a0393608@udtensorlab9>
2025-03-18 19:55:54 +0530

    release script update

--- scripts/git_subtree_add_update_all.sh
+++ scripts/git_subtree_add_update_all.sh
@@ -31,9 +31,9 @@
 
 RELEASE_BRANCH=${1-"r10.1"}
 
-GIT_REPOS=( edgeai-docs edgeai-modelzoo edgeai-modelmaker edgeai-modeloptimization edgeai-benchmark edgeai-torchvision edgeai-tensorvision edgeai-yolox edgeai-mmdetection edgeai-mmdetection3d edgeai-mmpose edgeai-hf-transformers edgeai-mmdeploy )
+GIT_REPOS=( edgeai-docs edgeai-modelzoo edgeai-modelmaker edgeai-modeloptimization edgeai-benchmark edgeai-torchvision edgeai-tensorvision edgeai-yolox edgeai-mmdetection edgeai-mmdetection3d edgeai-mmpose edgeai-hf-transformers edgeai-mmdeploy edgeai-yolox )
 # removed repositories
-# edgeai-modelutils edgeai-datasets edgeai-mmrazor edgeai-yolox
+# edgeai-modelutils edgeai-datasets edgeai-mmrazor 
 
 for GIT_REPO in "${GIT_REPOS[@]}"
 do
