4bb236f9d4f8884969d3ab90f032bbf4dce3834a
Manu <a0393608@udtensorlab9>
2025-05-02 21:38:24 +0530

    changing cuda version to 12.4

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ pip3 install --no-input cython wheel numpy==1.23.0
 
 ######################################################################
 echo "installing pytorch - use the applopriate index-url from https://pytorch.org/get-started/locally/"
-pip3 install --no-input torch==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cu118
+pip3 install --no-input torch==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cu124
 
 ######################################################################
 pip install -U openmim
