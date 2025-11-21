c6a2b1d9e42c65062e2932050005d417fd0e8b0a
Manu <a0393608@udtensorlab9>
2025-11-21 15:21:49 +0530

    formatted_narges - function corrected

--- edgeai_benchmark/utils/config_utils/misc_utils.py
+++ edgeai_benchmark/utils/config_utils/misc_utils.py
@@ -262,7 +262,7 @@ def formatted_nargs(nargs_list, delimiters=(' ', ',')):
         formatted_arg = []
         for arg in nargs_list:
             arg_list = arg.split(delimiter) if isinstance(arg, str) else [arg]
-            new_arg = [m.strip() for m in arg_list]
+            new_arg = [m.strip() if isinstance(m, str) else m for m in arg_list]
             formatted_arg.extend(new_arg)
         #
         nargs_list = formatted_arg
